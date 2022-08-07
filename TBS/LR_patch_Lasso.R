5ea3e11e785a88660c2d73622dcde75c0cd5298a
wane199 <wane199@outlook.com>
2022-08-08 00:34:07 +0800

    1

--- TBS/LR_patch_Lasso.R
+++ TBS/LR_patch_Lasso.R
@@ -5,7 +5,7 @@ str(dt)
 summary(dt)
 
 # 因子化
-VarsC <- names(dt[c(1:2, 4:12, 17:21)]) # 分类变量
+VarsC <- names(dt[c(1:2, 4:13, 15, 17:21)]) # 分类变量
 for (i in VarsC) {
   dt[, i] <- as.factor(dt[, i])
 } # 利用循环因子化
@@ -13,9 +13,6 @@ for (i in VarsC) {
 plot(density(dt$TBS), main = "Distribution of TBS")
 polygon(density(dt$TBS), col = "grey") # polygon()用于曲线内填充颜色
 abline(v = 1.34, lwd = 2, col = "skyblue") # abline（a, b）表示在图上添加一条y=a+bx的直线
-
-dt$TBS <- dt$TBS > 1.34
-summary(dt)
 
 library(caret)
 # 待筛选特征标准化
@@ -70,7 +67,7 @@ plot(performance(pred2, "tpr", "fpr"), add = T, colorize = T, lwd = 3)
 plot(performance(pred3, "tpr", "fpr"), add = T, colorize = T, lwd = 3)
 abline(a = 0, b = 1, lty = 2, lwd = 3, col = "black")
 
-varsU <- names(dt[, 2:21]) # 自变量
+varsU <- names(trainingset[, 2:21]) # 自变量
 Result <- c()
 for (i in 1:length(varsU)) {
   fit <- glm(substitute(Y ~ x, list(x = as.name(varsU[i]))), data = dt, family = binomial())
@@ -86,7 +83,7 @@ for (i in 1:length(varsU)) {
 Uni_log <- data.frame(Result[, c(1, 4:8)]) # 提取"P","OR","CIlower","CIupper"和变量名
 colnames(Uni_log)[2:5] <- c("P", "OR", "CIlower", "CIupper") # 变量重命名
 ExtractVar <- unique(Uni_log$Characteristics[Uni_log$"P" < 0.05]) # 提取有意义的变量
-write.csv(Uni_log, file = "Uni_log.csv") # 输出文档
+write.csv(Uni_log, file = "./TBS/Uni_log.csv") # 输出文档
 Uni_log
 
 # https://mp.weixin.qq.com/s?__biz=Mzg2MjU2NDQwMg==&mid=100008208&idx=1&sn=f725f48085617f41e02aa83e13fd2aae&chksm=4e07584d7970d15b4b2d1a274f186c0f1adde7f24bb8d295e2eba3e590c7c6635fa779af0166#rd
@@ -101,19 +98,19 @@ Uni_glm_model <-
     # 拟合结局和变量
     FML <- as.formula(paste0("Y==1~", x))
     # glm()逻辑回归
-    glm1 <- glm(FML, data = train, family = binomial)
+    glm1 <- glm(FML, data = trainingset, family = binomial)
     # 提取所有回归结果放入glm2中
     glm2 <- summary(glm1)
     # 1-计算OR值保留两位小数
-    OR <- round(exp(coef(glm1)), 2)
+    OR <- round(exp(coef(glm1)), 4)
     # 2-提取SE
-    SE <- glm2$coefficients[, 2]
-    # 3-计算CI保留两位小数并合并
-    CI5 <- round(exp(coef(glm1) - 1.96 * SE), 2)
-    CI95 <- round(exp(coef(glm1) + 1.96 * SE), 2)
+    SE <- glm2$coefficients[, 4]
+    # 3-计算CI保留四位小数并合并
+    CI5 <- round(exp(coef(glm1) - 1.96 * SE), 4)
+    CI95 <- round(exp(coef(glm1) + 1.96 * SE), 4)
     CI <- paste0(CI5, "-", CI95)
     # 4-提取P值
-    P <- round(glm2$coefficients[, 4], 2)
+    P <- round(glm2$coefficients[, 4], 4)
     # 5-将变量名、OR、CI、P合并为一个表，删去第一行
     Uni_glm_model <- data.frame(
       "Characteristics" = x,
@@ -126,7 +123,7 @@ Uni_glm_model <-
   }
 
 # 把它们放入variable.names中
-variable.names <- colnames(train)[c(2:21)]
+variable.names <- colnames(trainingset)[c(2:21)]
 variable.names
 
 # 变量带入循环函数
@@ -138,9 +135,10 @@ Uni_glm
 # 注意：这一步只是为了提取变量仅此而已
 variable.names
 paste0(variable.names, collapse = "+")
-names <- glm(Y == 1 ~ sex + age + Cre + eGFR + Urea + CysC + ALP + VD + PTH + Ca + P +
-  BMI + BMD + TBS + TscoreL1L4 + Dialysis_duration + Smoking + Drinking + DM + Drugs,
-data = train,
+
+names <- glm(Y == 1 ~ sex + age + Cre + eGFR + Urea + CysC + ALP + VD + PTH + Ca + P + BMI + BMD +
+  TBS + TscoreL1L4 + Dialysis_duration + Smoking + Drinking + DM + Drugs,
+data = trainingset,
 family = binomial
 )
 name <- data.frame(summary(names)$aliased)
@@ -152,11 +150,11 @@ Uni_glm <- Uni_glm[, -1]
 Uni_glm$P[Uni_glm$P == 0] <- "<0.001"
 Uni_glm
 # 保存为Excel
-write.csv(Uni_glm, "批量单因素回归三线表结果.csv")
+write.csv(Uni_glm, "./TBS/单因素回归三线表结果.csv")
 
 # 多因素logistic回归
-varsMul <- c("age", "sex", "P", "DM", "Ca", "TBS", "Dialysis_duration") # 需要进行多因素分析的变量，随机生成的数据单因素无意义，故强制纳入
-dataAM <- data.frame(subset(train, select = c("Y", varsMul[1:length(varsMul)]))) # 将因变量和要分析的自变量单独建库
+varsMul <- c("age", "sex", "P", "Ca", "eGFR", "ALP", "TBS") # 需要进行多因素分析的变量，随机生成的数据单因素无意义，故强制纳入
+dataAM <- data.frame(subset(trainingset, select = c("Y", varsMul[1:length(varsMul)]))) # 将因变量和要分析的自变量单独建库
 fitMul <- glm(Y ~ ., data = dataAM, family = binomial()) # 行多因素logistic回归分析
 fitSum <- summary(fitMul)
 ResultMul <- c() # 准备空向量，用来储存结果
@@ -164,13 +162,12 @@ ResultMul <- rbind(ResultMul, fitSum$coef)
 OR <- exp(fitSum$coef[, "Estimate"])
 ResultMul <- cbind(ResultMul, cbind(OR, exp(confint(fitMul))))
 ResultMul
-write.csv(ResultMul, file = "Mul_log.csv")
+write.csv(ResultMul, file = "./TBS/Mul_log.csv")
 
 # https://zhuanlan.zhihu.com/p/369933231
 # 全子集回归 | 最优子集筛选
 lmfit <- lm(Y == 1 ~ sex + age + Cre + eGFR + Urea + CysC + ALP + VD + PTH + Ca + P + BMI + TBS
   + Dialysis_duration + Smoking + Drinking + DM + Drugs, data = train)
-
 
 library(olsrr)
 # 全子集回归
@@ -181,9 +178,8 @@ plot(ols_step_all_possible(lmfit))
 ols_step_best_subset(lmfit)
 plot(ols_step_best_subset(lmfit))
 
-lmfitbm <- lm(Y ~ age + sex + P + DM + Ca + TBS + Dialysis_duration, data = train)
+lmfitbm <- lm(Y ~ age + sex + P + eGFR + Ca + TBS, data = train)
 summary(lmfitbm)
-
 
 library(bestglm)
 library(leaps)
@@ -196,16 +192,33 @@ bestglm(lgtdata, IC = "CV", family = binomial) ## Information criteria to use: "
 # https://mp.weixin.qq.com/s?__biz=MzIzNjk2NDg4NA==&mid=2247486273&idx=1&sn=a098f0f274cd72577fb8e077dfc797db&chksm=e8ce963adfb91f2cc557915fefd78b265b12172ae987d467c44e6403d6c8aed94460fd00a3e1&mpshare=1&scene=1&srcid=0805D2CX1m2beuBxiL39Q7LC&sharer_sharetime=1659707936766&sharer_shareid=e01bedee3072575c19cc0d90b2125a40#rd
 # 数据准备，切分数据集
 # 按7:3将数据集分割为训练集和测试集合
-dt <- read.csv("/home/wane/Desktop/TBS&Mon/BIAO/PTH1/CKD1-2.csv", header = T)
+rm(list = ls())
+write.csv(dt, "/home/wane/Desktop/TBS&Mon/BIAO/PTH1/CKD1-3.csv")
+dt <- read.csv("/home/wane/Desktop/TBS&Mon/BIAO/PTH1/CKD1-3.csv", header = T)
 dt <- dt[-1]
 str(dt)
 summary(dt)
+# 多分类变量进行哑变量编码(Dummy Encoding)/独热编码(One-Hot Encoding)
+# 构造分类变量
+dt$TBS <- ifelse(dt$TBS < 1.34, 0, 1)
+dt$BMI <- ifelse(dt$BMI < 24, 0, dt$BMI)
+dt$BMI <- ifelse(dt$BMI >= 24 & dt$BMI < 30, 1, dt$BMI)
+# dt$BMIc<-ifelse (dt$BMI>=30 & dt$BMI<35,2,dt$BMIc)
+# dt$BMIc<-ifelse (dt$BMI>=35 & dt$BMI<40,3,dt$BMIc)
+dt$BMI <- ifelse(dt$BMI >= 30, 2, dt$BMI)
+dt$Dialysis_duration <- ifelse(dt$Dialysis_duration > 0, 1, dt$Dialysis_duration)
+dt$TBS <- factor(dt$TBS)
+dt$BMI <- factor(dt$BMI)
+dt$Dialysis_duration <- factor(dt$Dialysis_duration)
+
+library(cattonum)
+dt.BMIc.onehot <- catto_onehot(dt, c(10, 22)) # 独热编码，相当于在x2的基础上增加参照水平的0/1赋值。cattonum包中的函数catto_dummy也可以进行哑变量编码
 dt$Y <- ifelse(dt$Y == "0", "No Fracture", "Fracture")
 # dt$Y <- as.factor(as.character(dt$Y))
 # dt$Y <- as.factor(dt$Y,levels = c(0, 1),
 #                   labels = c("No Fracture", "Fracture"))
 # 批量数值转因子
-for (i in names(dt)[c(1:2, 4:12, 17:21)]) {
+for (i in names(dt)[c(1:2, 4:13,15,17:21)]) {
   dt[, i] <- as.factor(dt[, i])
 }
 set.seed(123)
@@ -287,7 +300,7 @@ as_kable(tra, format = "latex")
 as_kable_extra(tra, format = "latex")
 # 基线特征描述统计
 library(autoReg)
-ft <- gaze(Y ~ ., data = trainingset) %>% myft()
+ft <- gaze(Y ~ ., data = testingset) %>% myft()
 ft
 library(rrtable)
 table2pptx(ft) # Exported table as Report.pptx
@@ -297,7 +310,7 @@ table2docx(tra, title = "Train", append = TRUE, vanilla = TRUE)
 trainingset$Y <- factor(trainingset$Y, labels = c("No Fracture", "Fracture"))
 ## setLabel()函数给变量名添加标签
 trainingset$Y <- setLabel(trainingset$Y, "Fracture Risk")
-fit <- glm(Y ~ age + Cre + eGFR + Urea + CysC + ALP + PTH + Ca + P + BMI + TBS + sex,
+fit <- glm(Y ~ age + eGFR + Ca + P + TBS + sex,
   data = trainingset, family = "binomial"
 )
 summary(fit)
@@ -332,7 +345,7 @@ ExpCatViz(
   target = "Y"
 )
 
-op <- par(mfrow = c(4, 4))
+op <- par(mfrow = c(4, 5))
 hist(trainingset, col = "lightblue", border = "pink")
 utils::str(hist(trainingset, col = "gray", labels = TRUE))
 r <- hist(sqrt(trainingset), breaks = 12, col = "lightblue", border = "pink")
@@ -356,7 +369,7 @@ ggbetweenstats(
   type = "np"
 )
 
-ExpNumViz(trainingset, target = "Y", Page = c(4, 4))
+ExpNumViz(trainingset, target = "Y", Page = c(1, 3))
 
 ## 生成解释变量和结局变量的矩阵格式，glmnet数据格式是矩阵
 Xtrain <- as.matrix(trainingset[, 2:21])
@@ -401,6 +414,213 @@ coef(lsocv, s = "lambda.1se") # 获取使模型偏差最小时λ值+一个标准
 cbind2(coef(lsocv, s = "lambda.min"), coef(lsocv, s = "lambda.1se")) # 合并显示
 
 
+## 构建响应变量和解释变量矩阵
+y <- as.matrix(trainingset[, 1])
+x1 <- as.matrix(trainingset[, c("age", "sex", "P", "DM", "Ca", "TBS", "Dialysis_duration")])
+# 解释变量全部为连续变量的多因素矩阵
+head(x1)
 
+x2 <- model.matrix(~ age + sex + P + DM + Ca + TBS + Dialysis_duration,
+  data = trainingset
+)[, -1]
+# 哑变量编码。构建多因素矩阵，model.matrix(~A+B+…)[,-1] 将为因子编码生成变量0/1哑变量，并保持连续变量不变。最后的[,-1]从 model.matrix 的输出中删除常数项
+head(x2)
+
+library(rms)
+dd <- datadist(trainingset) ## 设置数据环境
+options(datadist = "dd")
+
+fit1 <- glm(Y ~ age + P + Ca + TBS + sex + eGFR,
+  data = trainingset, family = "binomial"
+)
+fit2 <- step(fit1)
+summary(fit2)
+1 / exp(coef(fit2))
+
+# 拟合模型
+fit <- lrm(Y ~ age + P + Ca + TBS + sex + eGFR,
+  data = trainingset
+)
+fit
 # Alignment Diagram/Nomogram Plot
+nomogram <- nomogram(fit, # 模型名称
+                     lp = T, # 显示线性概率
+                     fun.at = c(0.1,0.3,0.5,0.7,0.9), # 坐标轴刻度
+                     conf.int = c(0.1,0.7), # 显示置信区间
+                     funlabel = "Risk", # 设置坐标轴名称
+                     fun = function(x) 1 / (1 + exp(-x))) # 逻辑回归计算公式
+# 绘制列线图
+plot(nomogram)
+# 设置因子的水平标签
+trainingset$sex <- factor(trainingset$sex,
+  levels = c(1, 0),
+  labels = c("Male", "Female")
+)
+# 设置变量的名称
+label(trainingset$sex) <- "Gender"
+label(trainingset$age) <- "Age"
+label(Affairs$religiousness) <- "宗教信仰"
+label(Affairs$rating) <- "婚姻自我评分"
+
+nom <- nomogram(fit,
+  fun = plogis, conf.int = c(0.1,0.7),
+  fun.at = c(.001, .01, .05, seq(.1, .9, by = .1), .95, .99, .999),
+  lp = F, funlabel = "Risk of Fracture"
+)
+plot(nom,
+     lplabel = "Linear Predictor", # 设置线性概率坐标轴名称
+     # fun.side = c(1,3,1,3,1,3), # 坐标轴刻度位置
+     col.grid = c("blue","yellow"), # 垂直参考线的颜色
+     col.conf = c("red","green"), # 设置置信区间的颜色
+     conf.space = c(0.1,0.5) # 设置置信区间条位置
+     )
+
 library(regplot)
+regplot(fit, # 模型名称
+        odds = T, # 设置OR显示
+        title = "Nomogram for Fracture Risk at CKD",
+        observation = trainingset[1, ], # 指定观测值
+        interval = "confidence", points=TRUE) # 最大刻度100
+
+# 常见列线图的绘制及自定义美化详细教程
+# https://mp.weixin.qq.com/s?__biz=MzU4OTc0OTg2MA==&mid=2247497910&idx=1&sn=350a4d6c689462d7337e04455912c8ce&chksm=fdca73bdcabdfaab401fab2a00a9a24a60e7e706675b774bd3d866f6c884cfc80c7a478e7403&mpshare=1&scene=1&srcid=0607mDKXD346ABXXHRc5Sn6I&sharer_sharetime=1654698032379&sharer_shareid=13c9050caaa8b93ff320bbf2c743f00b#rd
+
+# ROC曲线及PR曲线
+pre <- predict(fit1, type = "response")
+pre
+library(pROC)
+plot.roc(trainingset$Y, pre,
+  main = "ROC curve in Training set", percent = TRUE,
+  print.auc = TRUE,
+  ci = TRUE, of = "thresholds",
+  thresholds = "best",
+  print.thres = "best"
+)
+
+rocplot1 <- roc(trainingset$Y, pre)
+auc(rocplot1)
+ci.auc(rocplot1)
+# ROC详细结果
+roc.result <- coords(rocplot1,"best", ret = "all", transpose = F)
+as.matrix(roc.result)
+
+library(modEvA)
+aupr=AUC(obs=trainingset$Y, pred=trainingset$trainpredlab,
+         curve="PR", simplif=T, main="PR curve")
+
+# 混淆矩阵绘制
+require(caret)
+# 训练集预测概率
+trainpredprob <- predict(fit1, newdata = trainingset, type = "response")
+view(trainpredprob)
+# 训练集ROC
+trainroc <- roc(response = trainingset$Y, predict = trainpredprob)
+# 训练集ROC曲线
+plot(trainroc,
+  print.auc = TRUE,
+  auc.polygon = TRUE,
+  grid = T,
+  max.auc.polygon = T,
+  auc.polygon.col = "skyblue",
+  print.thres = T,
+  legacy.axes = T,
+  bty = "l"
+)
+# 约登法则
+bestp <- trainroc$thresholds[
+  which.max(trainroc$sensitivities + trainroc$specificities - 1)
+]
+bestp
+# 训练集预测分类
+trainpredlab <- as.factor(ifelse(trainpredprob > bestp, 1, 0))
+
+# 训练集混淆矩阵
+confusionMatrix(
+  data = trainpredlab, # 预测类别
+  reference = factor(trainingset$Y), # 实际类别
+  positive = "1",
+  mode = "everything"
+)
+# 校准度
+fitcal <- lrm(Y ~ age + P + Ca + TBS + sex + eGFR,
+                     data = trainingset, x=TRUE, y=TRUE)
+cal <- calibrate(fitcal, method = "boot", B = 1000)
+plot(cal,
+  xlab = "Nomogram Predicted Fracture",
+  ylab = "Actual Fracture", main = "Calibration Curve"
+)
+
+# 内部验证集Internal validation Discrimination
+ddist <- datadist(test)
+options(datadist = "ddist")
+test$sex.M <- as.factor(test$sex.M)
+test$sex.M <- as.numeric(as.character(test$sex.M))
+str(test)
+class(test$sex.M)
+pre1 <- predict(fit4, newdata = test)
+plot.roc(test$Y, pre1,
+  main = "ROC Curve", percent = TRUE,
+  print.auc = TRUE,
+  ci = TRUE, of = "thresholds",
+  thresholds = "best",
+  print.thres = "best"
+)
+rocplot2 <- roc(test$Y, pre1)
+ci.auc(rocplot2)
+
+# 混淆矩阵绘制
+require(caret)
+# 测试集预测概率
+testpredprob <- predict(fit4, newdata = test, type = "response")
+view(testpredprob)
+# 测试集ROC
+testroc <- roc(response = test$Y, predict = testpredprob)
+# 测试集ROC曲线
+plot(testroc,
+  print.auc = TRUE,
+  auc.polygon = TRUE,
+  grid = T,
+  max.auc.polygon = T,
+  auc.polygon.col = "skyblue",
+  print.thres = T,
+  legacy.axes = T,
+  bty = "l"
+)
+# 约登法则
+bestp <- testroc$thresholds[
+  which.max(testroc$sensitivities + testroc$specificities - 1)
+]
+bestp
+# 测试集预测分类
+testpredlab <- as.factor(ifelse(testpredprob > bestp, 1, 0))
+class(testpredlab)
+class(test$Y)
+# 测试集混淆矩阵
+confusionMatrix(
+  data = testpredlab, # 预测类别
+  reference = factor(test$Y), # 实际类别
+  positive = "1",
+  mode = "everything"
+)
+# Calibration
+fit5 <- lrm(test$Y ~ pre1, x = TRUE, y = TRUE)
+cal2 <- calibrate(fit5, method = "boot", B = 1000)
+plot(cal2,
+  xlab = "Nomogram Predicted Fracture",
+  ylab = "Actual Fracture", main = "Calibration Curve"
+)
+
+
+
+# Brier score
+library(rms)
+
+val.prob()
+
+# R2/AIC/BIC
+
+AIC()
+BIC()
+
+# ROC curve/C-index
+Y ~ age + sex + Ca + P + TBS + eGFR
