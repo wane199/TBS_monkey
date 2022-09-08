be7bdb0d8f27b93541ba1dcf0c81a271f323c251
wane <36815574+wane199@users.noreply.github.com>
2022-09-08 10:13:11 +0800

    2

---
+++ EP/ISBI_2017/r_scripts/brain_extraction_malf.R
@@ -0,0 +1,162 @@
+## ----setup, include=FALSE------------------------------------------------
+library(methods)
+knitr::opts_chunk$set(echo = TRUE, comment = "")
+
+## ----reading_in_image, message = FALSE-----------------------------------
+library(neurobase)
+t1_fname = "training01_01_mprage.nii.gz"
+t1 = neurobase::readnii(t1_fname)
+rt1 = robust_window(t1); 
+red0.5 = scales::alpha("red", 0.5) # for plotting later
+
+## ----t1_plot_robust------------------------------------------------------
+ortho2(rt1)
+
+## ----t1_naive_ss, eval = FALSE, cache = FALSE, message = FALSE, warning = FALSE----
+## library(fslr)
+## ss = fslbet(infile = t1_fname)
+
+## ----t1_naive_ss_run, echo = FALSE, cache = FALSE, message = FALSE, warning = FALSE----
+library(fslr)
+out_fname = "../output/naive_ss.nii.gz"
+if (!file.exists(out_fname)) {
+  ss = fslbet(infile = t1_fname)
+  writenii(ss, out_fname)
+} else {
+  ss = readnii(out_fname)
+}
+
+## ----t1_naive_plot_ss----------------------------------------------------
+ortho2(robust_window(ss))
+
+## ----t1_ss_plot----------------------------------------------------------
+ortho2(rt1, ss > 0, col.y = red0.5)
+
+## ----bc_show, eval = FALSE-----------------------------------------------
+## library(extrantsr)
+## bc_img = bias_correct(file = t1, correction = "N4")
+
+## ----bc_show_run, echo = FALSE-------------------------------------------
+bc_fname = "../output/training01_01_mprage_n4.nii.gz"
+bc_img = readnii(bc_fname)
+bc_img = robust_window(bc_img)
+
+## ----bc_bet, eval = FALSE, message = FALSE-------------------------------
+## bc_bet = fslbet(bc_img); ortho2(bc_img, bc_bet > 0, col.y = red0.5)
+
+## ----bc_bet_run, message = FALSE, echo = FALSE---------------------------
+out_fname = "../output/bc_bet_ss.nii.gz"
+if (!file.exists(out_fname)) {
+  bc_bet = fslbet(bc_img); 
+  writenii(bc_bet, out_fname)
+} else {
+  bc_bet = readnii(out_fname)
+}
+ortho2(bc_img, bc_bet > 0, col.y = red0.5)
+
+## ----t1_malf_ss, echo = TRUE, eval = FALSE-------------------------------
+## library(malf.templates) # load the data package
+## library(extrantsr)
+## timgs = mass_images(n_templates = 5) # let's register 5 templates
+## ss = extrantsr::malf(
+##   infile = bc_img,
+##   template.images = timgs$images,
+##   template.structs = timgs$masks,
+##   keep_images = FALSE # don't keep the registered images
+## )
+## mask = ss > 0
+
+## ----t1_malf_ss_run, echo = FALSE, message = FALSE-----------------------
+library(malf.templates)
+library(extrantsr)
+timgs = mass_images(n_templates = 5)
+outfile = "../output/training01_01_mprage_mask.nii.gz"
+if (!file.exists(outfile)) {
+  ss = malf(
+    infile = bc_fname, 
+    template.images = timgs$images, 
+    template.structs = timgs$masks,
+    keep_images = FALSE,
+    verbose = FALSE,
+    outfile = outfile
+  )
+} else {
+  ss = readnii(outfile)
+}
+mask = ss > 0
+
+## ----show_them, eval = FALSE---------------------------------------------
+## mask = readnii("training01_01_mprage_mask.nii.gz") # already computed
+
+## ----display_malf_result-------------------------------------------------
+ortho2(bc_img, mask, col.y = red0.5)
+
+## ------------------------------------------------------------------------
+library(kirby21.t1)
+t1_fname = get_t1_filenames()[1]
+t1 = readnii(t1_fname)
+
+## ----kirby21_t1_plot-----------------------------------------------------
+ortho2(robust_window(t1))
+
+## ----kirby21_t1_naive_ss_run, cache = FALSE, message = FALSE, echo = FALSE----
+out_fname = "../output/kirby_naive_ss.nii.gz"
+if (!file.exists(out_fname)) {
+  ss = fslbet(infile = t1_fname)
+  writenii(ss, out_fname)
+} else {
+  ss = readnii(out_fname)
+}
+ortho2(robust_window(ss))
+
+## ----kirby21_t1_naive_ss_show, eval = FALSE, cache = FALSE, message = FALSE----
+## ss = fslbet(infile = t1_fname); ortho2(robust_window(ss))
+
+## ----kirby21_bc_bet_show, message = FALSE, eval = FALSE------------------
+## bc_img = bias_correct(t1, correction = "N4");
+## bc_bet = fslbet(bc_img)
+## ortho2(robust_window(t1), bc_bet > 0, col.y = red0.5)
+
+## ----kirby21_bc_bet_run, message = FALSE, echo = FALSE-------------------
+out_fname = "../output/kirby_bc.nii.gz"
+if (!file.exists(out_fname)) {
+  bc_img = bias_correct(t1, correction = "N4"); 
+  writenii(bc_img, out_fname)
+} else {
+  bc_img = readnii(out_fname)
+}
+
+out_fname = "../output/kirby_bc_bet.nii.gz"
+if (!file.exists(out_fname)) {
+  bc_bet = fslbet(bc_img)  
+  writenii(bc_bet, out_fname)
+} else {
+  bc_bet = readnii(out_fname)
+}
+ortho2(robust_window(t1), bc_bet > 0, col.y = red0.5)
+
+## ---- eval = FALSE-------------------------------------------------------
+## ss = extrantsr::fslbet_robust(
+##   t1,
+##   remover = "double_remove_neck",
+##   correct = TRUE,
+##   correction = "N4",
+##   recog = TRUE)
+
+## ----t1_ss, cache = FALSE, echo = FALSE----------------------------------
+outfile = nii.stub(t1_fname, bn = TRUE)
+outfile = file.path("..", "output", paste0(outfile, "_SS.nii.gz"))
+if (!file.exists(outfile)) {
+  ss = extrantsr::fslbet_robust(t1_fname,
+    remover = "double_remove_neck",
+    outfile = outfile)
+} else {
+  ss = readnii(outfile)
+}
+
+## ----kirby21_t1_ss_plot_show, cache = TRUE, eval = FALSE-----------------
+## ortho2(ss)
+
+## ----kirby21_t1_ss_plot_run, echo = FALSE, cache = TRUE------------------
+ortho2(dropEmptyImageDimensions(ss))
+
---
+++ EP/ISBI_2017/r_scripts/intro.R
@@ -0,0 +1,48 @@
+# https://johnmuschelli.com/imaging_in_r/
+if (!"devtools" %in% installed.packages()[, "Package"]) {
+  install.packages("devtools")
+}
+if (!"ms.lesion" %in% installed.packages()[, "Package"]) {
+  devtools::install_github("muschellij2/ms.lesion")
+}
+source("https://neuroconductor.org/neurocLite.R")
+pkgs = c("neurobase", "fslr", "dcm2niir","divest", 
+         "RNifti", "oro.dicom", 
+         "oro.nifti", "WhiteStripe", "neurohcp", "papayar",
+         "papayaWidget", "oasis", "kirby21.t1")
+neuro_install(pkgs)
+
+# data for whitestripe
+library(dcm2niir); install_dcm2nii()
+library(WhiteStripe); download_img_data()
+
+neuro_install(c("ITKR", "ANTsRCore", "ANTsR", "extrantsr"),
+              upgrade_dependencies = FALSE)
+
+neuro_install("malf.templates")
+## ---- echo = FALSE-------------------------------------------------------
+library(methods)
+library(knitr)
+opts_chunk$set(comment = "")
+
+## ----readin, echo = FALSE, message=FALSE---------------------------------
+# install.packages("remotes")
+# remotes::install_github("muschellij2/ms.lesion")
+library(ms.lesion)
+library(neurobase)
+files = get_image_filenames_list_by_subject(type = "coregistered")
+files = files$training02
+img_fnames = files[c("MPRAGE", "T2", "FLAIR", "PD")]
+mask_fname = files["mask1"]
+brain_mask = readnii(files["Brain_Mask"])
+imgs = check_nifti(img_fnames)
+mask = readnii(mask_fname)
+zimgs = lapply(imgs, zscore_img, mask = brain_mask)
+inds = getEmptyImageDimensions(brain_mask)
+zimgs = lapply(zimgs, applyEmptyImageDimensions, inds = inds)
+mask = applyEmptyImageDimensions(mask, inds = inds)
+zimgs = c(zimgs, Lesion_Mask = list(mask * 10))
+xyz = xyz(mask)
+multi_overlay(zimgs, z = xyz[3], text = names(zimgs), 
+              text.x = 0.5, text.y = 1.25, text.cex = 2)
+
