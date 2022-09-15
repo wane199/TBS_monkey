30b8c66c21e9ca87db8b80ed87a39c28f376d70d
wane <36815574+wane199@users.noreply.github.com>
2022-09-15 15:51:01 +0800

    1

--- EP/EP_Cox_Nomo/heatmap.R
+++ EP/EP_Cox_Nomo/heatmap.R
@@ -2,7 +2,7 @@
 # 清理工作环境
 rm(list = ls())
 # 读入数据
-dt <- read.csv("/home/wane/Desktop/EP/Structured_Data/Task2/TLE234group.csv")
+dt <- read.csv("/Users/mac/Desktop/BLS-ep-pre/EP/Structured_Data/Task2/TLE234group.csv")
 train <- subset(dt, dt$Group == "Training")
 test <- subset(dt, dt$Group == "Test")
 
@@ -10,7 +10,8 @@ rownames(dt)=dt[,4]
 data <- as.matrix(dt[5:22])
 # t(data) # transpose the matrix with to swap X and Y axis.
 # Default Heatmap
-heatmap(data, Rowv = NA,  Colv = NA, scale = "row")
+heatmap(data, Rowv = NA, margins = c(3,3),
+        Colv = NA, scale = "column")
 # Use 'scale' to normalize
 heatmap(data, scale="column")
 # No dendrogram nor reordering for neither column or row
@@ -22,15 +23,15 @@ heatmap(data, Colv = NA, Rowv = NA, scale="column", col = terrain.colors(256))
 # 2: Rcolorbrewer palette
 library(RColorBrewer)
 coul <- colorRampPalette(brewer.pal(8, "PiYG"))(25)
-heatmap(data, Colv = NA, Rowv = NA, scale="column", margins = c(6,6), col = coul)
+heatmap(data, Colv = NA, Rowv = NA, scale="column", 
+        cex.axis=0.5,cex.lab=2,cex.main=3,
+        margins = c(5,5), col = coul)
+
 # Example: grouping from the first letter:
 my_group <- as.numeric(as.factor(substr(rownames(data), 1 , 1)))
 colSide <- brewer.pal(9, "Set1")[my_group]
 colMain <- colorRampPalette(brewer.pal(9, "Blues"))(50)
 heatmap(data, Colv = NA, Rowv = NA, scale="column" , RowSideColors=colSide, col=colMain)
-
-
-
 
 
 library(pheatmap)
@@ -73,5 +74,42 @@ plot_list <- list(p1[[4]], p2[[4]])
 
 grid.arrange(arrangeGrob(grobs= plot_list,ncol=2))
 
+# https://officeguide.cc/r-ggplot2-elegant-tiled-heat-maps-tutorial-examples/
+library(reshape2)
+library(ggplot2)
+# 準備原始資料
+x <- data.frame(scale(dt[5:22]))
+x$sub <- rownames(dt)
+# 將資料表轉為長型表格
+x.melt <- melt(x, id.vars = "sub")
 
+# 使用 ggplot 繪製熱圖
+ggplot(x.melt, aes(x = sub, y = variable, fill = value)) +
+  geom_tile(colour = "white", size = 0.25) + # 繪製熱圖
+  scale_y_discrete(expand = c(0,0)) +        # 移除多餘空白
+  scale_x_discrete(expand = c(0,0)) +        # 移除多餘空白
+  coord_fixed() +                            # 設定 X 與 Y 軸等比例
+  scale_fill_gradientn(colours = terrain.colors(10)) + # 設定色盤
+  theme(
+    legend.text = element_text(face="bold"), # 說明文字用粗體
+    axis.ticks = element_line(size=0.5),     # 座標軸上的刻度寬度
+    plot.background = element_blank(),       # 移除背景
+    panel.border = element_blank(),          # 移除邊框
+    axis.text.x = element_text(
+      angle = 90, vjust = 0.5, hjust = 1)    # X 軸文字轉向
+  )
 
+ggplot(x.melt, aes(x = variable, y = sub, fill = value)) +
+  geom_tile(colour = "white", size = 0.25) + # 繪製熱圖
+  scale_y_discrete(expand = c(0,0)) +        # 移除多餘空白
+  scale_x_discrete(expand = c(0,0)) +        # 移除多餘空白
+  coord_fixed() +                            # 設定 X 與 Y 軸等比例
+  scale_fill_gradientn(colours = terrain.colors(10)) + # 設定色盤
+  theme(
+    legend.text = element_text(face="bold"), # 說明文字用粗體
+    axis.ticks = element_line(size=0.5),     # 座標軸上的刻度寬度
+    plot.background = element_blank(),       # 移除背景
+    panel.border = element_blank(),          # 移除邊框
+    axis.text.x = element_text(
+      angle = 90, vjust = 0.5, hjust = 1)    # X 軸文字轉向
+  )
