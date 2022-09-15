7f14aeb6629d7918973f3801d807e461224544b6
wane <36815574+wane199@users.noreply.github.com>
2022-09-15 16:59:16 +0800

    1

--- EP/BLS_EP_files/EML/NMI.R
+++ EP/BLS_EP_files/EML/NMI.R
@@ -2,7 +2,21 @@
 install.packages("treeheatr")
 library(treeheatr)
 
-summary(iris)
+dt <- read.csv("/Users/mac/Desktop/BLS-ep-pre/EP/Structured_Data/PET-TLE234-radscore-RCS2.csv")
+summary(dt)
+dt <- dt[c(5:19)]
+heat_tree(dt, target_lab = "oneyr", task = 'classification')
 
-
+# 任意修改图片颜色
+heat_tree(dt, target_lab = "oneyr",
+          target_cols = c("royalblue1", "palegreen3")) # 修改颜色
+# 选择将变量全部在热图中展示
+heat_tree(dt, target_lab = "oneyr",
+          show_all_feats = TRUE)
+# 各个分类组别之间的距离加宽一些
+heat_tree(dt, target_lab = "oneyr",
+          panel_space = 0.03)   # 调整组别间的距离
+# 只想要决策树，也可以去掉热图部分图片： 
+heat_tree(dt, target_lab = "oneyr",show_all_feats = TRUE,
+          show = "heat-only")  # 只显示决策树/热图show = "heat-tree",
 
