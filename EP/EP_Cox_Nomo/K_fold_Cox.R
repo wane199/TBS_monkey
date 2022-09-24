b1745e22af09cff64f54659dc16a625c045c961a
wane <36815574+wane199@users.noreply.github.com>
2022-09-24 11:19:26 +0800

    1

--- EP/EP_Cox_Nomo/K_fold_Cox.R
+++ EP/EP_Cox_Nomo/K_fold_Cox.R
@@ -90,104 +90,132 @@ for (i in 1:1000) {
 # 将36个月的c指数放入res
 res$c_value36 <- with(res, c_value36)
 
-#5年C-index的2000次验证 
-###建空表
-c_value60<-as.numeric()
-#做成循环
+# 5年C-index的2000次验证
+### 建空表
+c_value60 <- as.numeric()
+# 做成循环
 for (i in 1:1000) {
   train <- dt[folds[[i]], ]
   test <- dt[-folds[[i]], ]
   model <- coxph(Surv(Follow_up_timemon, Rel._in_5yrs) ~ radscore + SGS + familial_epilepsy + Durmon + SE, data = train, x = T)
   mod <- cindex(list(model1 = model),
-                formula = Surv(Follow_up_timemon, Rel._in_5yrs) ~ 1,
-                data = test, eval.times = 60
+    formula = Surv(Follow_up_timemon, Rel._in_5yrs) ~ 1,
+    data = test, eval.times = 60
   )
   c_value60 <- append(c_value60, as.numeric(mod$AppCindex))
 }
-res$c_value60 <- with(res,c_value60) 
+res$c_value60 <- with(res, c_value60)
 
 # res表宽转长
 library(reshape2)
 res <- data.frame(res)
-res1<- melt(res,
-            measure.vars = c("auc_value36","auc_value60",
-                             "c_value36","c_value60"),
-            variable.name = "Sample",
-            value.name = "value")
-#需要转换的列名：measure.vars()  
-#新数据标签列名：variable.name()
-#新数数据数值名：value.name()      
+res1 <- melt(res,
+  measure.vars = c(
+    "auc_value36", "auc_value60",
+    "c_value36", "c_value60"
+  ),
+  variable.name = "Sample",
+  value.name = "value"
+)
+# 需要转换的列名：measure.vars()
+# 新数据标签列名：variable.name()
+# 新数数据数值名：value.name()
 
 # 交叉验证结果可视化
 # 1. 小提琴图
 library(ggplot2)
 library(ggprism)
-ggplot(res1, aes(Sample,value))+
-  geom_violin(aes(fill = Sample))+
-  geom_boxplot(width=0.1)+
-  theme_prism(base_size =15,border =T)+
-  theme(legend.position = "none")    
+ggplot(res1, aes(Sample, value)) +
+  geom_violin(aes(fill = Sample)) +
+  geom_boxplot(width = 0.1) +
+  theme_prism(base_size = 15, border = T) +
+  theme(legend.position = "none")
 
 # 2. 箱线图+散点图
-ggplot(data=res1)+ 
-  geom_boxplot(mapping=aes(x=Sample,y=value,colour =Sample),
-               alpha = 0.5,size=1.5,width = 0.6)+ 
-  geom_jitter(mapping=aes(x=Sample,y=value,colour =Sample), 
-              alpha = 0.3,size=2)+
-  scale_color_manual(limits=c("auc_value36","auc_value60",
-                              "c_value36","c_value60"), 
-                     values=c("#c1121f","#0466c8","#2e7542","#fca311"))+ 
-  theme_prism(base_size =15,border =T)+
-  theme(legend.position = "none")       
+ggplot(data = res1) +
+  geom_boxplot(
+    mapping = aes(x = Sample, y = value, colour = Sample),
+    alpha = 0.5, size = 1.5, width = 0.6
+  ) +
+  geom_jitter(
+    mapping = aes(x = Sample, y = value, colour = Sample),
+    alpha = 0.3, size = 2
+  ) +
+  scale_color_manual(
+    limits = c(
+      "auc_value36", "auc_value60",
+      "c_value36", "c_value60"
+    ),
+    values = c("#c1121f", "#0466c8", "#2e7542", "#fca311")
+  ) +
+  theme_prism(base_size = 15, border = T) +
+  theme(legend.position = "none")
 
 # 云雨图（小提琴+蜂图）
-#4.云雨图
+# 4.云雨图
 library(see)
-ggplot(res1, aes(x = Sample,y = value, fill = Sample)) +
-  geom_violindot(binwidth = 0.0025,dots_size =1,color =NA) +
-  coord_flip()+scale_fill_discrete()+ 
-  theme_prism(border =T)+theme(legend.position = "none")     
+ggplot(res1, aes(x = Sample, y = value, fill = Sample)) +
+  geom_violindot(binwidth = 0.0025, dots_size = 1, color = NA) +
+  coord_flip() +
+  scale_fill_discrete() +
+  theme_prism(border = T) +
+  theme(legend.position = "none")
 
 # 5. 云雨图（小提琴+散点）
 library(gghalves)
-#小提琴+散点
-ggplot(res1,aes(x = Sample,y = value, fill = Sample))+
+# 小提琴+散点
+ggplot(res1, aes(x = Sample, y = value, fill = Sample)) +
   geom_half_violin(aes(fill = Sample),
-                   position = position_nudge(x = .15, y = 0),side = 'r') +
-  geom_point(aes(x=as.numeric(Sample)+0.05,y =value,color =Sample),
-             position=position_jitter(width=0.05),size=0.25,shape = 20)+
-  coord_flip()+#转90度
-  theme_prism(border =T)+theme(legend.position = "none")    
+    position = position_nudge(x = .15, y = 0), side = "r"
+  ) +
+  geom_point(aes(x = as.numeric(Sample) + 0.05, y = value, color = Sample),
+    position = position_jitter(width = 0.05), size = 0.25, shape = 20
+  ) +
+  coord_flip() + # 转90度
+  theme_prism(border = T) +
+  theme(legend.position = "none")
 
 # 6. 云雨图-3（小提琴+箱图+散点）
 library(tidyverse)
 library(dplyr)
 library(ggsci)
 # 先计算均值等,报错重装dplyr
-summ_res1 <- res1 %>% 
-  group_by(Sample) %>% 
-  summarise(mean = mean(value),
-            sd = sd(value),
-            n = n()
-  ) %>% 
-  mutate(se = sd/sqrt(n),
-         Species = factor(Sample, levels = c("auc_value36","auc_value60","c_value36","c_value60"))
-  );summ_res1
+summ_res1 <- res1 %>%
+  group_by(Sample) %>%
+  summarise(
+    mean = mean(value),
+    sd = sd(value),
+    n = n()
+  ) %>%
+  mutate(
+    se = sd / sqrt(n),
+    Species = factor(Sample, levels = c("auc_value36", "auc_value60", "c_value36", "c_value60"))
+  )
+summ_res1
 
 
-##小提琴+箱线图+散点+误差
-ggplot(res1,aes(x = Sample,y = value, fill = Sample))+
-  geom_half_violin(aes(fill =Sample),position = position_nudge(x = .15, y = 0),
-                   adjust=1.5, trim=T, colour=NA, side = 'r') +
-  geom_point(aes(x = as.numeric(Sample)-0.1,y = value,color = Sample),
-             position = position_jitter(width = 0.05),size = .25, shape = 20) +
-  geom_half_boxplot(aes(x =as.numeric(Sample)+0.15,y = value, fill = Sample),
-                    width = 0.25,color = "black")+
-  geom_errorbar(data =summ_res1,aes(x =Sample,y = mean,group =Sample,
-                                    colour =Sample, ymin = mean-se, 
-                                    ymax = mean+se),width=0.05,
-                position=position_nudge(x = -0.01, y = 0))+
-  scale_color_jco()+
-  scale_fill_jco()+
-  coord_flip()+
-  theme_prism(border =T)+theme(legend.position = "none")
+## 小提琴+箱线图+散点+误差
+ggplot(res1, aes(x = Sample, y = value, fill = Sample)) +
+  geom_half_violin(aes(fill = Sample),
+    position = position_nudge(x = .15, y = 0),
+    adjust = 1.5, trim = T, colour = NA, side = "r"
+  ) +
+  geom_point(aes(x = as.numeric(Sample) - 0.1, y = value, color = Sample),
+    position = position_jitter(width = 0.05), size = .25, shape = 20
+  ) +
+  geom_half_boxplot(aes(x = as.numeric(Sample) + 0.15, y = value, fill = Sample),
+    width = 0.25, color = "black"
+  ) +
+  geom_errorbar(
+    data = summ_res1, aes(
+      x = Sample, y = mean, group = Sample,
+      colour = Sample, ymin = mean - se,
+      ymax = mean + se
+    ), width = 0.05,
+    position = position_nudge(x = -0.01, y = 0)
+  ) +
+  scale_color_jco() +
+  scale_fill_jco() +
+  coord_flip() +
+  theme_prism(border = T) +
+  theme(legend.position = "none")
