20463e936d98ba8528b40cafd8809d6f41661e70
wane199 <wane199@outlook.com>
2022-09-24 12:15:56 +0800

    1

--- TBS/平滑曲线1/GAMLSS.R
+++ TBS/平滑曲线1/GAMLSS.R
@@ -11,71 +11,76 @@ summary(dt)
 str(dt)
 
 # 构建LMS模型（该模型为GAMLSS方法下的一个特例）
-m0a<-lms(Frontal_Cortex,Age,data=dt,
-         trans.x=TRUE,k=3,families ="BCTo") #自由度选择2，方法为"BCCGo", "BCPEo", "BCTo"
+m0a <- lms(Frontal_Cortex, Age,
+  data = dt,
+  trans.x = TRUE, k = 3, families = "BCTo"
+) # 自由度选择2，方法为"BCCGo", "BCPEo", "BCTo"
 m0a
-m1 <- lms(y=Frontal_Cortex, x=Age, data=dt, trans.x=TRUE)
-m1 <- lms(y=Frontal_Cortex, x=Age, data=dt, n.cyc=30)
-m2 <- lms(y=Frontal_Cortex, x=Age, data=dt, method.pb="GAIC", k=log(610))
-mDEG_zip <- gamlss(formula = deg ~ pb(SE_score) + TL + species + sex + season + 
-                     year + re(random = ~1|code) +  re(random = ~1|station),
-                   family=ZIP(), data = node_dat,
-                   control = gamlss.control(n.cyc = 200))
-m=gamlss(Frontal_Cortex~cs(Age,df=3),family = BCT,data=dt)
+m1 <- lms(y = Frontal_Cortex, x = Age, data = dt, trans.x = TRUE)
+m1 <- lms(y = Frontal_Cortex, x = Age, data = dt, n.cyc = 30)
+m2 <- lms(y = Frontal_Cortex, x = Age, data = dt, method.pb = "GAIC", k = log(610))
+mDEG_zip <- gamlss(
+  formula = deg ~ pb(SE_score) + TL + species + sex + season +
+    year + re(random = ~ 1 | code) + re(random = ~ 1 | station),
+  family = ZIP(), data = node_dat,
+  control = gamlss.control(n.cyc = 200)
+)
+m <- gamlss(Frontal_Cortex ~ cs(Age, df = 3), family = BCT, data = dt)
 plot(m)
 # 分位数曲线，没有给出图例，因为没找到更改图例位置的参数，legend=TRUE有图例，ylim限定了y轴范围
-centiles(m,dt$Age,cent = c(1,3,5,10,25,50,75,90,95,97,99),legend = FALSE,ylab = "Frontal_Cortex",xlab = "Age",main = "",ylim=c(0.08,0.12))
+centiles(m, dt$Age, cent = c(1, 3, 5, 10, 25, 50, 75, 90, 95, 97, 99), legend = FALSE, ylab = "Frontal_Cortex", xlab = "Age", main = "", ylim = c(0.08, 0.12))
 
-gamlss(Frontal_Cortex~cs(Age,df=3),sigma.fo=log(Age),nu.fo=~1,tau.fo=~1,family=BCT,data=dt)
-       
-       
-Output1<-gamlss(Frontal_Cortex ~ Age, 
-                sigma.fo=~Age,
-                nu.fo=~1, 
-                family=BCCGo, 
-                data=dt)
-Output2<-gamlss(Frontal_Cortex ~ Age, 
-                sigma.fo=~Age,
-                nu.fo=~Age, 
-                family=BCCGo, 
-                data=dt)
-AIC(Output1,Output2)
+gamlss(Frontal_Cortex ~ cs(Age, df = 3), sigma.fo = log(Age), nu.fo = ~1, tau.fo = ~1, family = BCT, data = dt)
+
+
+Output1 <- gamlss(Frontal_Cortex ~ Age,
+  sigma.fo = ~Age,
+  nu.fo = ~1,
+  family = BCCGo,
+  data = dt
+)
+Output2 <- gamlss(Frontal_Cortex ~ Age,
+  sigma.fo = ~Age,
+  nu.fo = ~Age,
+  family = BCCGo,
+  data = dt
+)
+AIC(Output1, Output2)
 plot(Output1)
-op <- par(mfrow=c(1,2))
-wp(Output1) ; title(" (c) BCCG(mu, sigma)")
-wp(Output2) ; title(" (d) BCCG(mu, sigma, nu)")
+op <- par(mfrow = c(1, 2))
+wp(Output1)
+title(" (c) BCCG(mu, sigma)")
+wp(Output2)
+title(" (d) BCCG(mu, sigma, nu)")
 par(op)
 
 # 绘制平滑曲线，并添加数据分布热图
-plot<-plotSimpleGamlss(Frontal_Cortex,Age,m1,data=dt,
-                       x.val=seq(0,30,2), #定义数据分组
-                       xlim=c(0,30), #横轴的起止点为-2，23
-                       ylim=c(0,0.15),val=200) #纵轴的起止点为0，500
+plot <- plotSimpleGamlss(Frontal_Cortex, Age, m1,
+  data = dt,
+  x.val = seq(0, 30, 2), # 定义数据分组
+  xlim = c(0, 30), # 横轴的起止点为-2，23
+  ylim = c(0, 0.15), val = 200
+) # 纵轴的起止点为0，500
 
 # 图像另存为
-tiff(file = "Frontal_Cortex.tiff", res = 1400, width = 8800, 
-     height = 8800, compression = "lzw")
+tiff(
+  file = "Frontal_Cortex.tiff", res = 1400, width = 8800,
+  height = 8800, compression = "lzw"
+)
 plot
 dev.off()
 
-mod<-gamlss(Frontal_Cortex~pb(Age),sigma.fo=~pb(Age),
-            family=BCT, data=dt, method=mixed(1,20))
+mod <- gamlss(Frontal_Cortex ~ pb(Age),
+  sigma.fo = ~ pb(Age),
+  family = BCT, data = dt, method = mixed(1, 20)
+)
 plot(mod)
 # rm(mod)
-plot<-plotSimpleGamlss(Frontal_Cortex,Age,mod,data=dt,
-                       x.val=seq(0,30,2), #定义数据分组
-                       xlim=c(0,30), #横轴的起止点为-2，23
-                       ylim=c(0.075,0.115),val=50) #纵轴的起止点为0，500
+plot <- plotSimpleGamlss(Frontal_Cortex, Age, mod,
+  data = dt,
+  x.val = seq(0, 30, 2), # 定义数据分组
+  xlim = c(0, 30), # 横轴的起止点为-2，23
+  ylim = c(0.075, 0.115), val = 50
+) # 纵轴的起止点为0，500
 
-z.scores(mod, x=c(2,15,30,40),y=c(45,50,56,63))
-
-
-
-
-
-
-
-
-
-
-
+z.scores(mod, x = c(2, 15, 30, 40), y = c(45, 50, 56, 63))
