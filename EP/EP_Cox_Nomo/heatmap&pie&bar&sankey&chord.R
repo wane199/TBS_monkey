286577285c6fa0656c7abd3315a8927570e9cc2f
wane <36815574+wane199@users.noreply.github.com>
2022-10-17 00:25:59 +0800

    1

--- EP/EP_Cox_Nomo/heatmap&pie&bar&sankey&chord.R
+++ EP/EP_Cox_Nomo/heatmap&pie&bar&sankey&chord.R
@@ -250,10 +250,10 @@ library(patchwork)
 summary(train)
 
 #对参数设置尝试的代码
-train$Rad <- cut(train$radscore, breaks = c(-Inf, 0.1834, Inf), labels = c("1","2"), right=TRUE, include.lowest=TRUE)
-train$DurMonth <- cut(train$Durmon, breaks = c(-Inf, 108.00, Inf), labels = c("1","2"), right=TRUE, include.lowest=TRUE)
-test$Rad <- cut(test$radscore, breaks = c(-Inf, 0.1834, Inf), labels = c("1","2"), right=TRUE, include.lowest=TRUE)
-test$DurMonth <- cut(test$Durmon, breaks = c(-Inf, 108.00, Inf), labels = c("1","2"), right=TRUE, include.lowest=TRUE)
+train$Rad <- cut(train$radscore, breaks = c(-Inf, 0.1834, Inf), labels = c("0","1"), right=TRUE, include.lowest=TRUE)
+train$DurMonth <- cut(train$Durmon, breaks = c(-Inf, 108.00, Inf), labels = c("0","1"), right=TRUE, include.lowest=TRUE)
+test$Rad <- cut(test$radscore, breaks = c(-Inf, 0.1834, Inf), labels = c("0","1"), right=TRUE, include.lowest=TRUE)
+test$DurMonth <- cut(test$Durmon, breaks = c(-Inf, 108.00, Inf), labels = c("0","1"), right=TRUE, include.lowest=TRUE)
 
 dt_re1 <- melt(dt[c(1, 4, 5, 6, 7, 12, 14:15, 17)], id = c("ID"))
 dt_re1$value <- as.factor(dt_re1$value)
@@ -296,26 +296,30 @@ a / b + plot_layout(guides='collect') + plot_annotation(tag_levels = 'A')
 library(corrplot)
 library(circlize)
 library(Hmisc)
+
 # 读取数据
-rt <- read.csv(file.choose(), header = T)
-rownames(dt) <- dt[, 4]
-rt <- as.matrix(dt[5:22])
+# rt <- read.csv(file.choose(), header = T)
+rownames(train) <- train[, 4]
+rt <- as.matrix(train[c(6, 7, 12, 14:15, 17)])
+rownames(test) <- test[, 4]
+rt1 <- as.matrix(test[c(6, 7, 12, 14:15, 17)])
+
 # 计算指标间相关性
-cor1 <- cor(rt)
+cor1 <- cor(rt1)
 # 显示P值
-rt <- as.matrix(rt)
-p <- rcorr(rt)
+rt <- as.matrix(rt1)
+p <- rcorr(rt1)
 p
 # 设置图形颜色
 col <- c(rgb(1, 0, 0, seq(1, 0, length = 32)), rgb(0, 1, 0, seq(0, 1, length = 32)))
 # 删掉相关性=1的数据
 cor1[cor1 == 1] <- 0
 c1 <- ifelse(c(cor1) >= 0, rgb(1, 0, 0, abs(cor1)), rgb(0, 1, 0, abs(cor1)))
-col1 <- matrix(c1, nc = ncol(rt))
+col1 <- matrix(c1, nc = ncol(rt1))
 # 绘制和弦图
 par(mar = c(2, 2, 2, 4))
 circos.par(gap.degree = c(3, rep(2, nrow(cor1) - 1)), start.degree = 180)
-chordDiagram(cor1, grid.col = rainbow(ncol(rt)), col = col1, transparency = 0.5, symmetric = T)
+chordDiagram(cor1, grid.col = rainbow(ncol(rt1)), col = col1, transparency = 0.5, symmetric = T)
 par(xpd = T)
 # colorlegend(col,labels=c(1,0,-1))
 colorlegend(col, vertical = T, labels = c(1, 0, -1), xlim = c(1.1, 1.3), ylim = c(-0.4, 0.4))
