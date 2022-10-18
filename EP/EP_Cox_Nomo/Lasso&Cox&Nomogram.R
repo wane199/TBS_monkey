31d44cffbcce56a346736ef468fb79b69da628de
wane <36815574+wane199@users.noreply.github.com>
2022-10-18 10:29:42 +0800

    1

--- EP/EP_Cox_Nomo/Lasso&Cox&Nomogram.R
+++ EP/EP_Cox_Nomo/Lasso&Cox&Nomogram.R
@@ -22,7 +22,10 @@ library(My.stepwise)
 # write.csv(dt,"/home/wane/Desktop/EP/结构化数据/TableS1-2.csv", row.names = FALSE)
 # dt <- read.csv("/home/wane/Desktop/EP/Structured_Data/Task2/PT_radiomic_features_temporal_ind2.csv")
 dt <- read.csv("C:/Users/wane199/Desktop/EP/Structured_Data/Task2/TLE234group.csv")
+dt <- read.csv("/Users/mac/Desktop/BLS-ep-pre/EP/Structured_Data/Task2/TLE234group.csv")
+
 dt0 <- read.csv("./EP/EP_Cox_Nomo/TLE234-rad.csv")
+table(dt$Freq)
 
 dt <- dt[c(-1:-2,-4)]
 dt <- as.data.frame(dt)
