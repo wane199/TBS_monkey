caec811ead8eec5393a3eb6594c81148fbfa65c2
wane199 <wane199@outlook.com>
2023-01-18 12:03:21 +0800

    0118

--- TBS/Monkey_fitting/Bland_Altman.R
+++ TBS/Monkey_fitting/Bland_Altman.R
@@ -2,24 +2,52 @@
 rm(list = ls())
 dev.off() # clear plots
 cat("\014") # ctrl+L
-?cat
+? cat
+pacman::p_load(psych)
 pacman::p_unload()
+pacman::p_help(ggseg, web = TRUE) # web=TRUE, build.pdf
 library(readxl)
 library(ggplot2)
 library(ggExtra)
 set.seed(123)
 
-BAdata2 <- read_xlsx("C:/Users/wane1/Documents/file/TBS&Mon/Monkey/Ziqing/vali.xlsx")
+BAdata2 <-
+  read_xlsx("C:/Users/wane1/Documents/file/TBS&Mon/Monkey/Ziqing/vali.xlsx")
 # BAdata2 <- BAdata2[,c(-2,-5)]
 summary(BAdata2)
-BAdata2 <- transform(BAdata2, diff = Measured_TFM - Predicted_TFM, meand = (Measured_TFM + Predicted_TFM) / 2)
+BAdata2 <-
+  transform(
+    BAdata2,
+    diff = Measured_TFM - Predicted_TFM,
+    meand = (Measured_TFM + Predicted_TFM) / 2
+  )
 
 BAplot <- ggplot(BAdata2, aes(meand, diff)) +
-  geom_point(size = 2, shape = 21, colour = "blue") +
-  geom_hline(yintercept = 0, lty = 3, lwd = 1, color = "pink") +
-  geom_hline(yintercept = mean(BAdata2$diff), lty = 1, lwd = 1, color = "black") +
-  geom_hline(yintercept = mean(BAdata2$diff) + 1.96 * sd(BAdata2$diff), linetype = 3, color = "grey") +
-  geom_hline(yintercept = mean(BAdata2$diff) - 1.96 * sd(BAdata2$diff), linetype = 3, color = "grey") +
+  geom_point(size = 2,
+             shape = 21,
+             colour = "blue") +
+  geom_hline(
+    yintercept = 0,
+    lty = 3,
+    lwd = 1,
+    color = "pink"
+  ) +
+  geom_hline(
+    yintercept = mean(BAdata2$diff),
+    lty = 1,
+    lwd = 1,
+    color = "black"
+  ) +
+  geom_hline(
+    yintercept = mean(BAdata2$diff) + 1.96 * sd(BAdata2$diff),
+    linetype = 3,
+    color = "grey"
+  ) +
+  geom_hline(
+    yintercept = mean(BAdata2$diff) - 1.96 * sd(BAdata2$diff),
+    linetype = 3,
+    color = "grey"
+  ) +
   labs(x = "mean of TFM and TAT4M", y = "") +
   theme_classic() +
   theme(
@@ -30,20 +58,38 @@ BAplot <- ggplot(BAdata2, aes(meand, diff)) +
   #       axis.title.x=element_text(size=18,face="bold"),
   #       axis.title.y =element_text(size=18,face="bold"))+
   theme(
-    axis.text = element_text(size = 10, face = "bold"), axis.ticks.length = unit(-0.25, "cm"),
+    axis.text = element_text(size = 10, face = "bold"),
+    axis.ticks.length = unit(-0.25, "cm"),
     axis.text.x = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm")),
     axis.text.y = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
   ) +
   scale_x_continuous(limits = c(0, 5)) +
   scale_y_continuous(limits = c(-1.0, 1.5)) +
-  geom_text(x = 4.8, y = 0.9, label = "+1.96SD=0.83", size = 5) +
-  geom_text(x = 4.8, y = -0.9, label = "-1.96SD=-0.77", size = 5) +
-  geom_text(x = 4.8, y = 0.1, label = "Mean=0.03", size = 5)
+  geom_text(x = 4.8,
+            y = 0.9,
+            label = "+1.96SD=0.83",
+            size = 5) +
+  geom_text(
+    x = 4.8,
+    y = -0.9,
+    label = "-1.96SD=-0.77",
+    size = 5
+  ) +
+  geom_text(x = 4.8,
+            y = 0.1,
+            label = "Mean=0.03",
+            size = 5)
 # geom_text(x=155, y=-18, label="Mean=0.03 \n 95%CI(-16.9,-0.9) ",size=5)
 BAplot
-ggsave(BAplot, file = "/Users/mac/Desktop/Nomo-TBS/TBS&Mon/Monkey/Ziqing/B_A_1.tiff", device = "tiff", dpi = 300)
+ggsave(BAplot,
+       file = "/Users/mac/Desktop/Nomo-TBS/TBS&Mon/Monkey/Ziqing/B_A_1.tiff",
+       device = "tiff",
+       dpi = 300)
 # Bland-Altman Plot的packages还有qwraps2、PairedData和blandr
-ggMarginal(BAplot, type = "histogram", bins = 20, fill = "grey")
+ggMarginal(BAplot,
+           type = "histogram",
+           bins = 20,
+           fill = "grey")
 # type的类型包括density, histogram, boxplot, violin
 # BAdata+theme( panel.background = element_rect(fill = "white"))
 
@@ -58,38 +104,41 @@ p0 <- ggplot(BAdata2, aes(Predicted_TFM, Measured_TFM)) +
   theme_classic() +
   scale_x_continuous(expand = c(0, 0), breaks = seq(0, 5, 1)) +
   scale_y_continuous(expand = c(0, 0)) +
-  stat_smooth(method = lm, se = TRUE, formula = my.formula) +
+  stat_smooth(method = lm,
+              se = TRUE,
+              formula = my.formula) +
   theme(
-    axis.text = element_text(size = 10, face = "bold"), axis.ticks.length = unit(-0.15, "cm"),
+    axis.text = element_text(size = 10, face = "bold"),
+    axis.ticks.length = unit(-0.15, "cm"),
     axis.text.x = element_text(margin = unit(c(0.3, 0.3, 0.3, 0.3), "cm")),
     axis.text.y = element_text(margin = unit(c(0.3, 0.3, 0.3, 0.3), "cm"))
   )
 p0
 # https://cloud.tencent.com/developer/article/1854758
-lm <- ggplot(data = BAdata2, aes(x = Predicted_TFM, y = Measured_TFM)) +
+lm <-
+  ggplot(data = BAdata2, aes(x = Predicted_TFM, y = Measured_TFM)) +
   geom_smooth(
     method = "lm",
-    se = FALSE, color = "black",
+    se = FALSE,
+    color = "black",
     formula = my.formula
   ) +
-  stat_poly_eq(
-    formula = my.formula,
-    aes(label = paste(..eq.label..,
-      ..rr.label..,
-      sep = "~~~"
-    )),
-    parse = TRUE
-  ) +
-  geom_point(size = 2, shape = 21, colour = "black") +
-  coord_cartesian(
-    ylim = c(0, 4.000),
-    xlim = c(0, 4.000)
-  ) +
+  stat_poly_eq(formula = my.formula,
+               aes(label = paste(..eq.label..,
+                                 ..rr.label..,
+                                 sep = "~~~")),
+               parse = TRUE) +
+  geom_point(size = 2,
+             shape = 21,
+             colour = "black") +
+  coord_cartesian(ylim = c(0, 4.000),
+                  xlim = c(0, 4.000)) +
   # scale_y_continuous(breaks = c(0,100,200,300))+
   # scale_x_continuous(breaks = c(0,25,50,75,100,105))+#x轴比上个图多个105
   theme_bw() +
   theme(
-    axis.text = element_text(size = 10, face = "bold"), axis.ticks.length = unit(-0.25, "cm"),
+    axis.text = element_text(size = 10, face = "bold"),
+    axis.ticks.length = unit(-0.25, "cm"),
     axis.text.x = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm")),
     axis.text.y = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
   ) +
@@ -98,8 +147,12 @@ lm <- ggplot(data = BAdata2, aes(x = Predicted_TFM, y = Measured_TFM)) +
     color = "black",
     # fill="black",
     alpha = .7,
-    size = 0.7, se = T,
+    size = 0.7,
+    se = T,
     formula = y ~ x
   ) +
   theme(panel.grid = element_blank())
-ggsave(lm, file = "/Users/mac/Desktop/Nomo-TBS/TBS&Mon/Monkey/Ziqing/lm_TFM.tiff", device = "tiff", dpi = 300)
+ggsave(lm,
+       file = "/Users/mac/Desktop/Nomo-TBS/TBS&Mon/Monkey/Ziqing/lm_TFM.tiff",
+       device = "tiff",
+       dpi = 300)
--- TBS/Monkey_fitting/GAMLSS.R
+++ TBS/Monkey_fitting/GAMLSS.R
@@ -3,45 +3,88 @@
 library(gamlss)
 library(gamlss.util)
 
-dt <- read.csv("/Users/mac/Desktop/Nomo-TBS/TBS&Mon/Monkey/QIANG/PartⅠ猴脑体积发育数据分析/Gender.csv")
-dt <- read.csv("/home/wane/Desktop/TBS&Mon/Monkey/QIANG/PartⅠ猴脑体积发育数据分析/L&R.csv")
+dt <-
+  read.csv("/Users/mac/Desktop/Nomo-TBS/TBS&Mon/Monkey/QIANG/PartⅠ猴脑体积发育数据分析/Gender.csv")
+dt <-
+  read.csv("/home/wane/Desktop/TBS&Mon/Monkey/QIANG/PartⅠ猴脑体积发育数据分析/L&R.csv")
 dt$Sex <- as.factor(dt$Sex)
 dt$Side <- as.factor(dt$Side)
 summary(dt)
 str(dt)
 
 # 构建LMS模型（该模型为GAMLSS方法下的一个特例）
-m0a <- lms(Frontal_Cortex, Age,
+m0a <- lms(
+  Frontal_Cortex,
+  Age,
   data = dt,
-  trans.x = TRUE, k = 3, families = "BCTo"
+  trans.x = TRUE,
+  k = 3,
+  families = "BCTo"
 ) # 自由度选择2，方法为"BCCGo", "BCPEo", "BCTo"
 m0a
-m1 <- lms(y = Frontal_Cortex, x = Age, data = dt, trans.x = TRUE)
-m1 <- lms(y = Frontal_Cortex, x = Age, data = dt, n.cyc = 30)
-m2 <- lms(y = Frontal_Cortex, x = Age, data = dt, method.pb = "GAIC", k = log(610))
+m1 <- lms(
+  y = Frontal_Cortex,
+  x = Age,
+  data = dt,
+  trans.x = TRUE
+)
+m1 <- lms(y = Frontal_Cortex,
+          x = Age,
+          data = dt,
+          n.cyc = 30)
+m2 <-
+  lms(
+    y = Frontal_Cortex,
+    x = Age,
+    data = dt,
+    method.pb = "GAIC",
+    k = log(610)
+  )
 mDEG_zip <- gamlss(
   formula = deg ~ pb(SE_score) + TL + species + sex + season +
     year + re(random = ~ 1 | code) + re(random = ~ 1 | station),
-  family = ZIP(), data = node_dat,
+  family = ZIP(),
+  data = node_dat,
   control = gamlss.control(n.cyc = 200)
 )
-m <- gamlss(Frontal_Cortex ~ cs(Age, df = 3), family = BCT, data = dt)
+m <-
+  gamlss(Frontal_Cortex ~ cs(Age, df = 3),
+         family = BCT,
+         data = dt)
 plot(m)
 # 分位数曲线，没有给出图例，因为没找到更改图例位置的参数，legend=TRUE有图例，ylim限定了y轴范围
-centiles(m, dt$Age, cent = c(1, 3, 5, 10, 25, 50, 75, 90, 95, 97, 99), legend = FALSE, ylab = "Frontal_Cortex", xlab = "Age", main = "", ylim = c(0.08, 0.12))
+centiles(
+  m,
+  dt$Age,
+  cent = c(1, 3, 5, 10, 25, 50, 75, 90, 95, 97, 99),
+  legend = FALSE,
+  ylab = "Frontal_Cortex",
+  xlab = "Age",
+  main = "",
+  ylim = c(0.08, 0.12)
+)
 
-gamlss(Frontal_Cortex ~ cs(Age, df = 3), sigma.fo = log(Age), nu.fo = ~1, tau.fo = ~1, family = BCT, data = dt)
+gamlss(
+  Frontal_Cortex ~ cs(Age, df = 3),
+  sigma.fo = log(Age),
+  nu.fo = ~ 1,
+  tau.fo = ~ 1,
+  family = BCT,
+  data = dt
+)
 
 
-Output1 <- gamlss(Frontal_Cortex ~ Age,
-  sigma.fo = ~Age,
-  nu.fo = ~1,
+Output1 <- gamlss(
+  Frontal_Cortex ~ Age,
+  sigma.fo = ~ Age,
+  nu.fo = ~ 1,
   family = BCCGo,
   data = dt
 )
-Output2 <- gamlss(Frontal_Cortex ~ Age,
-  sigma.fo = ~Age,
-  nu.fo = ~Age,
+Output2 <- gamlss(
+  Frontal_Cortex ~ Age,
+  sigma.fo = ~ Age,
+  nu.fo = ~ Age,
   family = BCCGo,
   data = dt
 )
@@ -55,32 +98,50 @@ title(" (d) BCCG(mu, sigma, nu)")
 par(op)
 
 # 绘制平滑曲线，并添加数据分布热图
-plot <- plotSimpleGamlss(Frontal_Cortex, Age, m1,
+plot <- plotSimpleGamlss(
+  Frontal_Cortex,
+  Age,
+  m1,
   data = dt,
-  x.val = seq(0, 30, 2), # 定义数据分组
-  xlim = c(0, 30), # 横轴的起止点为-2，23
-  ylim = c(0, 0.15), val = 200
+  x.val = seq(0, 30, 2),
+  # 定义数据分组
+  xlim = c(0, 30),
+  # 横轴的起止点为-2，23
+  ylim = c(0, 0.15),
+  val = 200
 ) # 纵轴的起止点为0，500
 
 # 图像另存为
 tiff(
-  file = "Frontal_Cortex.tiff", res = 1400, width = 8800,
-  height = 8800, compression = "lzw"
+  file = "Frontal_Cortex.tiff",
+  res = 1400,
+  width = 8800,
+  height = 8800,
+  compression = "lzw"
 )
 plot
 dev.off()
 
-mod <- gamlss(Frontal_Cortex ~ pb(Age),
+mod <- gamlss(
+  Frontal_Cortex ~ pb(Age),
   sigma.fo = ~ pb(Age),
-  family = BCT, data = dt, method = mixed(1, 20)
+  family = BCT,
+  data = dt,
+  method = mixed(1, 20)
 )
 plot(mod)
 # rm(mod)
-plot <- plotSimpleGamlss(Frontal_Cortex, Age, mod,
+plot <- plotSimpleGamlss(
+  Frontal_Cortex,
+  Age,
+  mod,
   data = dt,
-  x.val = seq(0, 30, 2), # 定义数据分组
-  xlim = c(0, 30), # 横轴的起止点为-2，23
-  ylim = c(0.075, 0.115), val = 50
+  x.val = seq(0, 30, 2),
+  # 定义数据分组
+  xlim = c(0, 30),
+  # 横轴的起止点为-2，23
+  ylim = c(0.075, 0.115),
+  val = 50
 ) # 纵轴的起止点为0，500
 
 z.scores(mod, x = c(2, 15, 30, 40), y = c(45, 50, 56, 63))
@@ -97,8 +158,8 @@ plot(grip ~ log(age), data = grip)
 log(exp(3))
 log10(1e7) # = 7
 
-x <- 10^-(1+2*1:9)
-cbind(x, log(1+x), log1p(x), exp(x)-1, expm1(x))
+x <- 10 ^ -(1 + 2 * 1:9)
+cbind(x, log(1 + x), log1p(x), exp(x) - 1, expm1(x))
 
 # fit a standard Gaussian GAM
 fit1 <- gamV(grip ~ s(age),
@@ -106,13 +167,16 @@ fit1 <- gamV(grip ~ s(age),
              aViz = list(nsim = 50))
 
 check1D(fit1, "age") + l_gridCheck1D(sd)  # stan dev
-check1D(fit1, "age") + l_densCheck( )  
+check1D(fit1, "age") + l_densCheck()
 
 
 # using a gaulss model
-fit2 <- gamV(list(grip ~ s(age), ~ (age)),
-             data = grip, family = gaulss,
-             aViz = list(nsim = 50))
+fit2 <- gamV(
+  list(grip ~ s(age), ~ (age)),
+  data = grip,
+  family = gaulss,
+  aViz = list(nsim = 50)
+)
 
 check1D(fit2, "age") + l_gridCheck1D(sd)  # stan dev
 
@@ -122,16 +186,19 @@ check1D(fit2, "age") + l_gridCheck1D(skewness)  # stan dev
 
 # shash model
 library(mgcFam)
-fit3 <- gamV(list(grip ~ s(age), ~ (age), ~ (age), ~ 1),
-             data = grip, family = shash,
-             aViz = list(nsim = 50))
+fit3 <- gamV(
+  list(grip ~ s(age), ~ (age), ~ (age), ~ 1),
+  data = grip,
+  family = shash,
+  aViz = list(nsim = 50)
+)
 
 check1D(fit3, "age") + l_gridCheck1D(skewness)  # stan dev
 
 AIC(fit1, fit2, fit3)
 
 
-# Quantile modeling: 
+# Quantile modeling:
 library(mgcViz)
 library(SemiPar)
 data(age.income)
@@ -143,20 +210,41 @@ fitQ <- qgamV(income ~ s(age), data = age.income, qu = 0.5)
 ft <- predict(fitQ, se = T)
 
 ord <- order(age.income$age)
-plot(age.income$age, age.income$income, xlab = 'Age', las = 1.0,
-     ylab = "Income (CAD)", col = "grey")
+plot(
+  age.income$age,
+  age.income$income,
+  xlab = 'Age',
+  las = 1.0,
+  ylab = "Income (CAD)",
+  col = "grey"
+)
 lines(age.income$age[ord], ft$fit[ord], col = 2)
-lines(age.income$age[ord], (ft$fit + 2 * ft$se.fit)[ord], col = 2, lty = 2)
-lines(age.income$age[ord], (ft$fit - 2 * ft$se.fit)[ord], col = 2, lty = 2)
+lines(age.income$age[ord],
+      (ft$fit + 2 * ft$se.fit)[ord],
+      col = 2,
+      lty = 2)
+lines(age.income$age[ord],
+      (ft$fit - 2 * ft$se.fit)[ord],
+      col = 2,
+      lty = 2)
 
 
-fitQ <- mqgamV(income ~ s(age), data = age.income, qu = c(0.1, 0.25, 0.5, 0.75, 0.9))
+fitQ <-
+  mqgamV(income ~ s(age),
+         data = age.income,
+         qu = c(0.1, 0.25, 0.5, 0.75, 0.9))
 plot(fitQ)
 plotRGL(fitQ)
-plot(age.income$age, age.income$income, xlab = 'Age', las = 1.0,
-     ylab = "Income (CAD)", col = "grey")
+plot(
+  age.income$age,
+  age.income$income,
+  xlab = 'Age',
+  las = 1.0,
+  ylab = "Income (CAD)",
+  col = "grey"
+)
 
-for (ii in 1:5){
+for (ii in 1:5) {
   ft <- predict(fitQ[[ii]], se = TRUE)
   lines(age.income$age[ord], ft$fit[ord], col = 1)
 }
@@ -171,14 +259,16 @@ summary(fitQ[[1]])
 
 library(gamlss)
 data(abdom)
-h1<-gamlss(y~cs(x), family=LOGNO, data=abdom)#fits the log-Normal distribution
-h2<-gamlss(y~cs(x), family=LNO, data=abdom)  #should be identical to the one above
+h1 <-
+  gamlss(y ~ cs(x), family = LOGNO, data = abdom)#fits the log-Normal distribution
+h2 <-
+  gamlss(y ~ cs(x), family = LNO, data = abdom)  #should be identical to the one above
 # to change to square root transformation, i.e. fix nu=0.5
-h3<-gamlss(y~cs(x), family=LNO, data=abdom, nu.fix=TRUE, nu.start=0.5)
-
-
-
-
-
-
-
+h3 <-
+  gamlss(
+    y ~ cs(x),
+    family = LNO,
+    data = abdom,
+    nu.fix = TRUE,
+    nu.start = 0.5
+  )
