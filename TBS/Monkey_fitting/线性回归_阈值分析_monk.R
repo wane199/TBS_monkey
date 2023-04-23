e75ee1a8942a1a597a9659895c099a8fa777039d
wane199 <wane199@outlook.com>
2023-04-23 23:47:31 +0800

    0423

--- TBS/Monkey_fitting/线性回归_阈值分析_monk.R
+++ TBS/Monkey_fitting/线性回归_阈值分析_monk.R
@@ -12,8 +12,8 @@ theme_set(theme_classic() + theme(legend.position = "bottom"))
 
 # dt <- read.csv("jixian.csv")
 dt <- read.csv("./TBS/Monkey_fitting/T1_TBV_1209.csv") # , sep = '\t'
-dt <- read.csv("C:\\Users\\wane1\\Documents\\file\\TBS&Mon\\Monkey\\QIANG\\0417\\T1_TBV.csv", sep = ';', fileEncoding = "GBK")
-write.csv(dt,'C:\\Users\\wane1\\Documents\\file\\TBS&Mon\\Monkey\\QIANG\\0417\\T1_TBV.csv')
+dt <- read.csv("C:\\Users\\wane1\\Documents\\file\\TBS&Mon\\Monkey\\QIANG\\0417\\T1_TBVratio_L&R.csv", sep = ';', fileEncoding = "GBK")
+# write.csv(dt,'C:\\Users\\wane1\\Documents\\file\\TBS&Mon\\Monkey\\QIANG\\0417\\T1_TBV.csv')
 # TLM <- read_excel("/home/wane/Desktop/TBS/TLMey/BMC.xlsx")
 # 数据探索EDA
 dt <- dt[c(-1, -2, -3)]
@@ -247,13 +247,13 @@ ggplot(data = dt, mapping = aes(x = Age, y = TBV, colour = Sex)) +
     formula = my.formula, parse = TRUE
   )
 # 置信区间带
-p2 <- ggplot(dt, mapping = aes(x = Age, y = TBV, colour = Side, fill = Side, linetype = Side)) +
-  # ylab(bquote(TBV/BW(cm^3/kg)))  + # 上下标 xlab("") + scale_fill_nejm() + scale_colour_nejm() +
-  scale_x_continuous(expand = c(0, 0), breaks = seq(0, 30, 1)) + # scale_y_continuous(expand = c(0,0)) +
+p2 <- ggplot(dt, mapping = aes(x = Age, y = TBV)) + # , colour = Side, fill = Side, linetype = Side
+  ylab(bquote(TBV(cm^3)))  + xlab("Age(year)") + # scale_fill_nejm() + scale_colour_nejm() +
+  scale_x_continuous(expand = c(0, 0), breaks = seq(0, 31, 1)) + scale_y_continuous(expand = c(0, 0), breaks = seq(45, 90, 5)) +
   # stat_cor(aes(), label.x = 3) + geom_vline(aes(xintercept=8.0),linetype=4,col="red") +
-  geom_smooth(method = mgcv::gam, formula = y ~ s(x, k = 4), se = T) + # ylab(bquote(TBV/BW(cm^3/kg))) +
-  geom_point(aes(colour = Side, shape = Side, fill = Side), size = 2) +
-  coord_trans(x = squash_axis(1, 99, 30)) # 局部压缩坐标轴
+  geom_smooth(method = mgcv::gam, formula = y ~ s(x, k = 3), se = T, colour = "black") + # ylab(bquote(TBV/BW(cm^3/kg))) +
+  geom_point(aes(), size = 2) + # colour = Side, shape = Side, fill = Side
+  # coord_trans(x = squash_axis(1, 99, 30)) # 局部压缩坐标轴
   theme(
     axis.text = element_text(size = 10, face = "bold"), axis.ticks.length = unit(-0.25, "cm"),
     axis.text.x = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm")),
@@ -267,24 +267,26 @@ p2
 # for循环
 theme_set(theme_classic() + theme(legend.position = "bottom"))
 plot_list <- list()
-for (i in 4:ncol(dt)) {
+for (i in 3:ncol(dt)) {
   print(p <- ggplot(dt, aes_string(x = "Age", y = colnames(dt)[i], colour = "Side", fill = "Side", linetype = "Side")) +
-    scale_x_continuous(expand = c(0, 0), breaks = seq(0, 30, 1)) +
+    scale_x_continuous(limits = c(0,30), expand = c(0, 0), breaks = seq(0, 30, 2)) +
     geom_smooth(method = mgcv::gam, formula = y ~ s(x, k = 4), se = T) +
-    geom_point(aes(colour = Side, shape = Side, fill = Side), size = 2) +
+    geom_point(aes(colour = Side, shape = Side, fill = Side), size = 1.5, shape = 21) +
+    ylab(bquote('Vr_ref Whole')) + xlab("Age(year)") + # Volume(cm^3)
+    scale_fill_brewer(palette = "Paired") + 
+    ggtitle(paste0(colnames(dt)[i])) + theme(plot.title = element_text(hjust = 0.5)) + 
     theme(
       axis.text = element_text(size = 10, face = "bold"), axis.ticks.length = unit(-0.25, "cm"),
       axis.text.x = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm")),
       axis.text.y = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
     ))
-  plot_list[[i - 3]] <- p
+  plot_list[[i - 2]] <- p
 }
 # 拼图
 # library(cowplot)
 # combined_plot <- cowplot::plot_grid(plotlist = plot_list, align = "h", nrow = 4)
 # legend <- get_legend(plot_list[[13]] + guides(color = guide_legend(nrow = 1)) + theme(legend.position = "bottom"))
 # plot_grid(combined_plot, legend,ncol=1,rel_heights = c(1, .1))
-
 library("patchwork")
 wrap_plots(plot_list, byrow = T, nrow = 4) + plot_annotation(tag_levels = "A") +
   plot_layout(guides = "collect")
@@ -306,9 +308,67 @@ ggplot(data = dt, mapping = aes(x = Age, y = TBV, colour = Sex)) +
 ggpar(p, palette = "jco")
 
 # [combine into single plot](https://www.math.pku.edu.cn/teachers/lidf/docs/Rbook/html/_Rbook/ggplot2.html)
+ggplot(dt, mapping = aes(x = Age, y = TBV)) + # , colour = Side, fill = Side, linetype = Side
+  ylab(bquote(Volume(cm^3))) + xlab("Age(year)") + 
+  scale_x_continuous(expand = c(0, 0), breaks = seq(0, 31, 1)) + scale_y_continuous(expand = c(0, 0), breaks = seq(45, 90, 5)) +
+  # stat_cor(aes(), label.x = 3) + geom_vline(aes(xintercept=8.0),linetype=4,col="red") +
+  geom_smooth(method = mgcv::gam, formula = y ~ s(x, k = 3), se = T, colour = "black") + # ylab(bquote(TBV/BW(cm^3/kg))) +
+  geom_point(aes(), size = 2, shape = 1) + # colour = Side, shape = Side, fill = Side
+  # coord_trans(x = squash_axis(1, 99, 30)) # 局部压缩坐标轴
+  theme(
+    axis.text = element_text(size = 10, face = "bold"), axis.ticks.length = unit(-0.25, "cm"),
+    axis.text.x = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm")),
+    axis.text.y = element_text(margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"))
+  )
+
+summary(dt)
+my.formula <- y ~ s(x, k = 3, bs = "cs")
+p6 <- ggplot(dt, aes(Age, CSF.TBV)) + # dt.summary
+  geom_point(aes(), alpha = 1.0, size = 1.5) + # colour = Sex,shape = Sex
+  theme_classic() +
+  ylab(bquote(Ratio)) + xlab("Age(year)") + # Volume(cm^3) TBV(cm^3) TBV.BW(cm^3/kg) Weight(kg) SUVr_whole_refPons Whole(KBq/cc)
+  scale_x_continuous(limits = c(0,30), breaks = seq(0, 30, 2), expand = c(0, 0)) + # expand = c(0, 0),
+  scale_y_continuous(limits = c(0.150,0.165), breaks = seq(0.150, 0.165, 0.002), expand = c(0, 0)) + # expand = c(0, 0),
+  # geom_vline(xintercept = 5.0, colour = "#990000", linetype = "dashed") +
+  stat_smooth(method = mgcv::gam, se = TRUE, colour = "black", formula = my.formula) +
+  # stat_smooth(method = mgcv::gam, se = TRUE, formula = y ~ s(x, bs = "cs")) +
+  geom_smooth(colour = "black",
+    data = dt, mapping = aes(x = Age, y = CSF.TBV), # , colour = Sex
+    method = "gam", formula = my.formula
+  ) + ggtitle("CSF_ratio") + theme(plot.title = element_text(hjust = 0.5)) + #设置标题居中
+  theme(
+    legend.position = "bottom",
+    axis.text = element_text(size = 10, face = "bold"), axis.ticks.length = unit(-0.15, "cm"),
+    axis.text.x = element_text(margin = unit(c(0.3, 0.3, 0.3, 0.3), "cm")),
+    axis.text.y = element_text(margin = unit(c(0.3, 0.3, 0.3, 0.3), "cm"))
+  )
+p6
+
+p4 <- ggplot(dt, aes(Age, TBV)) + # dt.summary
+  geom_point(aes(colour = Sex,shape = Sex), alpha = 1.0, size = 1.5) +
+  theme_classic() +
+  ylab(bquote(Volume(cm^3))) + xlab("Age(year)") + # TBV(cm^3) TBV.BW(cm^3/kg) Weight(kg) SUVr_whole_refPons Whole(KBq/cc)
+  scale_x_continuous(limits = c(0,30), breaks = seq(0, 30, 2), expand = c(0, 0)) + # expand = c(0, 0),
+  scale_y_continuous(limits = c(50.0,90.0), breaks = seq(50.0, 90.0, 5.0), expand = c(0, 0)) + # expand = c(0, 0),
+  # geom_vline(xintercept = 5.0, colour = "#990000", linetype = "dashed") +
+  # stat_smooth(method = mgcv::gam, se = TRUE, colour = "black", formula = my.formula) +
+  # stat_smooth(method = mgcv::gam, se = TRUE, formula = y ~ s(x, bs = "cs")) +
+  geom_smooth(
+    data = dt, mapping = aes(x = Age, y = TBV, colour = Sex),
+    method = "gam", formula = my.formula
+  ) +
+  theme(
+    legend.position = "bottom",
+    axis.text = element_text(size = 10, face = "bold"), axis.ticks.length = unit(-0.15, "cm"),
+    axis.text.x = element_text(margin = unit(c(0.3, 0.3, 0.3, 0.3), "cm")),
+    axis.text.y = element_text(margin = unit(c(0.3, 0.3, 0.3, 0.3), "cm"))
+  )
+p4
+p2
+
 library("patchwork")
-p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 +
-  p11 + p12 + p13 + plot_annotation(tag_levels = "A") +
+p1 + p2 + p3 + p4 + p5 + p6 + # + p7 + p8 + p9 + p10 + p11 + p12 + p13 + 
+  plot_annotation(tag_levels = "A") +
   plot_layout(guides = "collect")
 
 p1 + p2 + p3 + p4 + plot_annotation(tag_levels = "A") +
