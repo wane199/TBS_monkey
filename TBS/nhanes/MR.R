0e77fd3eb579df6158eaa6ab29383ae2dc855a19
wane199 <wane199@outlook.com>
2023-08-06 10:48:44 +0800

    0806

---
+++ TBS/nhanes/MR.R
@@ -0,0 +1,28 @@
+# Mendelian Randomization Analyses（https://www.bilibili.com/video/BV1Vu411H7EH/?spm_id_from=pageDriver&vd_source=23f183f0c5968777e138f31842bde0a0）
+# 
+library(TwoSampleMR)
+##两样本MR(在线这取)
+#提取BMI的SNP
+BMI <- extract_instruments(outcomes = 'ieu-a-2')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
