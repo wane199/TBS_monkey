076a2a262d375167b84848b6472972ad7a251398
wane199 <wane199@outlook.com>
2023-08-06 18:04:52 +0800

    0806

--- TBS/nhanes&MR/MR.R
+++ TBS/nhanes&MR/MR.R
@@ -6,13 +6,54 @@
 # install.packages("devtools")
 # devtools::install_github("MRCIEU/TwoSampleMR")
 library(TwoSampleMR)
+library(doParallel)
 
 ## 两样本MR(在线提取)
 # 读取数据与统计分析
 # 从数据库获取结局相关的GWAS
-## Get GWAS data from the IEU GWAS database that rooted in the package 
+## Get GWAS data from the IEU GWAS database that rooted in the package
 ao <- available_outcomes()
-#> API: public: http://gwas-api.mrcieu.ac.uk/
+# getwd()
+# write.csv(ao, "C:\\Users\\wane1\\Documents\\file\\sci\\Shuntak\\MR\\ao_2023_08_06.csv")
+ao <- read.csv("C:\\Users\\wane1\\Documents\\file\\sci\\Shuntak\\MR\\ao_2023_08_06.csv")
+# 根据id和trait提取组学数据
+# [代谢物] #  a b c d四个数据来源
+metabolites <- ao[grep("met", ao$id), ]
+# [蛋白质]
+protein <- ao[grep("prot", ao$id), ]
+# [菌群]就是Mibiogen上面的 #详细着菌群推文
+bacteria <- ao[grep("Gut microbiota abundance", ao$trait), ]
+# [eQTL]
+eQTL <- ao[grep("eqtl", ao$id), ]
+## 提取FinnGen的数据信息
+finn <- ao[grep("finn", ao$id), ]
+## 获取研究id的样本量信息等
+ao[ao$id == "ieu-a-7", c("sample_size", "ncase", "ncontrol")]
+
+## 提取工具变量（以eqtl为例）
+results <- extract_instruments(outcomes = eQTL$id, p1 = 1e-5, clump = TRUE)
+# 如频繁网络中断，可for循环追加
+# 保留，下次可以直接调用，进行不同结局探索
+df_dat_exp <-rbind(results,res)
+
+# for循环追加
+df_dat_exp = NULL
+for(i in 1:1000){
+  res <- extract_instruments(
+    outcomes=eQTL$id[i],p1 = 1e-5,
+    clump=TRUE)
+  #保留，下次可以直接调用，迹行不同结同探素
+  df_dat_exp <- rbind(df_dat_exp,res)
+}
+
+saveRDS(df_dat_exp,"C:\\Users\\wane1\\Documents\\file\\sci\\Shuntak\\MR\\eQTL_exp_dat.rds")
+df_dat_exp <- readRDS("C:\\Users\\wane1\\Documents\\file\\sci\\Shuntak\\MR\\eQTL_exp_dat.rds")
+
+
+
+
+
+
 ao_outcome <- ao[grepl("epilepsy", ao$trait), ]
 View(ao_outcome)
 
@@ -41,13 +82,13 @@ res_loo <- mr_leaveoneout(dat)
 
 # 可视化
 # scatter plot
-p1 <-mr_scatter_plot(res, dat)
+p1 <- mr_scatter_plot(res, dat)
 length(p1) # to see how many plots are there
 p1[[1]]
 
 # forest plot
-res_single <- mr_singlesnp(dat)#,all_method=c("mr_ivw", "mr_two_sample_ml")) to specify method used
-p2 <- mr_forest_plot(res_single) 
+res_single <- mr_singlesnp(dat) # ,all_method=c("mr_ivw", "mr_two_sample_ml")) to specify method used
+p2 <- mr_forest_plot(res_single)
 p2[[1]]
 
 ## funnel plot for all
@@ -56,18 +97,20 @@ p3[[1]]
 
 ### save images ##########
 library(ggplot2)
-ggsave(p1[[1]], file="mr_scatter_plot.png", 
-       width = 7, height=7, dpi=900)
+ggsave(p1[[1]],
+  file = "mr_scatter_plot.png",
+  width = 7, height = 7, dpi = 900
+)
 
 # 6. 结果整合
 mr_report(dat)
 
 ## To combine all resultsres<-mr(dat)
-het<-mr_heterogeneity(dat)
-plt<-mr_pleiotropy_test(dat)
-sin<-mr_singlesnp(dat)
-all_res<-combine_all_mrresults(res,het,plt,sin,ao_slc=T,Exp=T,split.exposure=F,split.outcome=T)
-head(all_res[,c("Method","outcome","exposure","nsnp","b","se","pval","intercept","intercept_se","intercept_pval","Q","Q_df","Q_pval","consortium","ncase","ncontrol","pmid","population")])
+het <- mr_heterogeneity(dat)
+plt <- mr_pleiotropy_test(dat)
+sin <- mr_singlesnp(dat)
+all_res <- combine_all_mrresults(res, het, plt, sin, ao_slc = T, Exp = T, split.exposure = F, split.outcome = T)
+head(all_res[, c("Method", "outcome", "exposure", "nsnp", "b", "se", "pval", "intercept", "intercept_se", "intercept_pval", "Q", "Q_df", "Q_pval", "consortium", "ncase", "ncontrol", "pmid", "population")])
 
 # 7. Variance explained
 out <- directionality_test(dat)
@@ -75,13 +118,23 @@ library(knitr)
 kable(out)
 
 mr_steiger(
-  p_exp = dat$pval.exposure, 
-  p_out = dat$pval.outcome, 
-  n_exp = dat$samplesize.exposure, 
-  n_out = dat$samplesize.outcome, 
-  r_xxo = 1, 
+  p_exp = dat$pval.exposure,
+  p_out = dat$pval.outcome,
+  n_exp = dat$samplesize.exposure,
+  n_out = dat$samplesize.outcome,
+  r_xxo = 1,
   r_yyo = 1,
-  r_exp=0)
+  r_exp = 0
+)
+
+
+
+
+
+
+
+
+
 
 
 
