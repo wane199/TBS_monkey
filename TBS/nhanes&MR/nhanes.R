3b08c5645b212bfdd6eb0b3e8ea811cc8f1654c5
wane199 <wane199@outlook.com>
2023-10-07 22:27:39 +0800

    1007

--- TBS/nhanes&MR/nhanes.R
+++ TBS/nhanes&MR/nhanes.R
@@ -1,11 +1,11 @@
 # https://mp.weixin.qq.com/s?__biz=MzI1NjM3NTE1NQ==&mid=2247486750&idx=1&sn=90c3338d3a010e024252687b32207246&chksm=ea26ed02dd516414ef982e116c1a1f114a5c72b395854c41b7f4692d2e28919b9993a425bcdd&mpshare=1&scene=1&srcid=11029kwnODSUuMBjcI9ptQHa&sharer_sharetime=1667362719667&sharer_shareid=13c9050caaa8b93ff320bbf2c743f00b#rd
 # [NHANES, National Health and Nutrition Examination Survey](https://wwwn.cdc.gov/nchs/nhanes/Default.aspx)
-library(haven)
-library(nhanesA)
-library(tidyverse)
+library(haven) # CRAN v2.5.3
+library(nhanesA) # CRAN v0.7.4
+library(tidyverse) # CRAN v2.0.0
 
 mydata <- read_xpt("/home/wane/Downloads/P_DEMO.XPT") # NHANES 2017-March 2020 Pre-Pandemic Demographics Data
-mydata1 <- nhanes("DEMO_E") # NHANES 2007-2008 Demographics Data
+mydata1 <- nhanes("DEMO_E") # NHANES 2007-2008 Demographics Data；_H：2013-2014
 
 # 对照变量说明提取需要的变量
 dat1 <- mydata1 %>% select(
@@ -16,8 +16,9 @@ dat1 <- mydata1 %>% select(
   DMDMARTL, # 婚姻状况
   WTINT2YR, WTMEC2YR, # 权重
   SDMVPSU, # psu
-  SDMVSTRA
-) # strata
+  SDMVSTRA  # strata
+) 
+
 # 关键的血糖和肺功能的指标，在化验室指标
 xuetang <- nhanes("GLU_E")
 # 对数据进行提取，序列号提取，
@@ -27,7 +28,7 @@ xuetang1 <- xuetang %>% select(
   LBDINSI, # 胰岛素( pmmol/L)
   PHAFSTHR # 餐后血糖
 )
-#
+# BPX_E:血压，SMQ_E:抽烟，ALQ_E:饮酒
 # 同理依次取糖化血红蛋白、肺功能数据
 tanghuadb <- nhanes("GHB_E")
 tanghuadb1 <- tanghuadb %>% select(
@@ -53,10 +54,9 @@ hdata <- plyr::join_all(list(dat1, xuetang1, tanghuadb1, feihuoliang1, dxaspn1),
 getwd()
 write.csv(hdata, file = "./TBS/nhanes/07-08.csv", row.names = F)
 
-#######################################################
-# 基线表绘制(table1)
-library(tableone)
-library(survey)
+###### 基线表绘制(table1) ######
+library(tableone) # CRAN v0.13.2
+library(survey) # CRAN v4.2-1
 bc <- read.csv("./TBS/nhanes/07-08.csv", sep = ",", header = TRUE)
 glimpse(bc)
 Hmisc::describe(bc)
@@ -92,9 +92,9 @@ Svytab2
 #####################################
 # 缺失值
 # https://www.yisu.com/zixun/444909.html
-library(lattice)
-library(mice)
-library(VIM)
+library(lattice) # CRAN v0.21-8
+library(mice) # CRAN v3.16.0 # CRAN v3.16.0
+library(VIM) # CRAN v6.2.2
 
 summary(bc)
 dim(bc)
@@ -145,9 +145,9 @@ for (i in 1:length(bc$ Ozone)) {
 
 # Plot incomplete or imputed data
 # load packages
-library(ggplot2)
-library(mice)
-library(ggmice)
+library(ggplot2) # CRAN v3.4.3 # CRAN v3.4.3
+library(mice) # CRAN v3.16.0 # CRAN v3.16.0
+library(ggmice) # CRAN v0.1.0
 # load some data
 dat <- boys
 # visualize the incomplete data
@@ -156,49 +156,60 @@ ggmice(dat, aes(age, bmi)) + geom_point()
 # impute the incomplete data
 imp <- mice(dat, m = 1, seed = 1, printFlag = FALSE)
 # visualize the imputed data
-ggmice(imp, aes(age, bmi)) + geom_point() 
+ggmice(imp, aes(age, bmi)) + geom_point()
 
 ##################
 # [nhanes数据库挖掘教程3--对数据进行多重插补](https://mp.weixin.qq.com/s?__biz=MzI1NjM3NTE1NQ==&mid=2247487020&idx=1&sn=9d504788a6909af3f797e86bbfdba8f7&chksm=ea26ee30dd516726ec22d640de1c0ef07fed8aa1c897918413b06a84a81a5b4cde311d389709&mpshare=1&scene=1&srcid=1209ZZUWTDh4pyjNkbKHLWbW&sharer_sharetime=1670587658361&sharer_shareid=13c9050caaa8b93ff320bbf2c743f00b#rd)
-library(mi)
+library(mi) # CRAN v1.1
 
 # 列出缺失值列表，提前处理好分类变量
 mdf <- missing_data.frame(bc)
 show(mdf)
 
 # 更改变量类型
-mdf <- change(mdf, y = c("RIDRETH1"), what = "type",
-              to = c("unorder"))
-mdf <- change(mdf, y = c("RIAGENDR"), what = "type",
-              to = c("binary"))
+mdf <- change(mdf,
+  y = c("RIDRETH1"), what = "type",
+  to = c("unorder")
+)
+mdf <- change(mdf,
+  y = c("RIAGENDR"), what = "type",
+  to = c("binary")
+)
 summary(mdf)
 hist(mdf)
 image(mdf)
 # {naniar}:让NA（缺失值）可见的多种图～
-library(naniar)
-library(ggplot2)
+library(naniar) # CRAN v1.0.0
+library(ggplot2) # CRAN v3.4.3 # CRAN v3.4.3
 # 大致查看一下包含缺失值的变量
-n_var_miss(mdf);gg_miss_which(mdf)
+n_var_miss(mdf)
+gg_miss_which(mdf)
 # 再查看所有变量的缺失值个数：
 gg_miss_var(mdf)
 # 缺失值所占百分比：
 gg_miss_var(mdf,
-            show_pct = TRUE)
+  show_pct = TRUE
+)
 vis_miss(bc)
 
 gg_miss_span(bc, LBDINSI, span_every = 1) +
   scale_x_continuous(breaks = seq(83, 1)) +
-  scale_y_continuous(breaks = c(0,1)) +
+  scale_y_continuous(breaks = c(0, 1)) +
   labs(
     subtitle = "LBDINSI",
-    x = "Observations") +
+    x = "Observations"
+  ) +
   theme_dark()
 # 包含缺失值的变量们的交集情况，就可以使用Upset plot：
 gg_miss_upset(mdf)
 
-ggplot(bc,
-       aes(x = LBDINSI,
-           y = DMDMARTL)) +
+ggplot(
+  bc,
+  aes(
+    x = LBDINSI,
+    y = DMDMARTL
+  )
+) +
   geom_point() +
   theme_bw()
 
@@ -211,7 +222,7 @@ ggplot(bc, aes(x = LBDINSI, y = DMDMARTL)) +
 imputation <- mi(mdf)
 imputation1 <- mi(mdf, n.chains = 5)
 
-round(mipply(imputation, mean, to.matrix = T),3)
+round(mipply(imputation, mean, to.matrix = T), 3)
 image(imputation)
 
 # 提取数据
@@ -222,11 +233,3 @@ image(a1)
 
 
 #####################################
-
-
-
-
-
-
-
-
